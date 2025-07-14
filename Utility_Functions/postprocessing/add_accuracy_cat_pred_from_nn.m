59a96a9ec686e07e4d39551c072bb98b78696e49
Luis David Davila <lddavila@miners.utep.edu>
2025-07-14 14:03:22 -0600

    added presorted table

--- Utility_Functions/postprocessing/add_accuracy_cat_pred_from_nn.m
+++ Utility_Functions/postprocessing/add_accuracy_cat_pred_from_nn.m
@@ -3,5 +3,6 @@ acc_pred_nn_struct = importdata(config.FP_TO_GRADES_ACC_PRED_WITH_RANK_NN);
 acc_pred_nn = acc_pred_nn_struct.net;
 sliced_bp_table = slice_table_for_parallel_processing(blind_pass_table,[]);
 for i=1:size(sliced_bp_table,1)
+    
 end
 end
