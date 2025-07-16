f16274e56a23de54a890825abfc782265ebeff70
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-07-16 16:41:17 -0600

    added debug message

--- clustering-master/util/downsample_spikes.m
+++ clustering-master/util/downsample_spikes.m
@@ -7,6 +7,13 @@ function downsampled = downsample_spikes(s, num_original, ir, config)
     downsampled = nan(numwires, numspikes, lastidx);
     
     chunksize = 250;
+    disp("smoothrange")
+    display(smoothrange);
+    disp("numdp")
+    display(numdp);
+    disp("waves");
+    display(waves)
+    
     for wire = 1:numwires
         waves = shiftdim(s(wire, :, :), 1)';
         smoothwaves = interp1(1:numdp, waves, smoothrange);
