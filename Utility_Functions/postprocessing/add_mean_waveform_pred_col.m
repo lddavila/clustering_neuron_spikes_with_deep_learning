e6e700d46ad2c34f9d18b750c2c6a1e959775698
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-07-20 15:25:37 -0600

    added skip

--- Utility_Functions/postprocessing/add_mean_waveform_pred_col.m
+++ Utility_Functions/postprocessing/add_mean_waveform_pred_col.m
@@ -1,7 +1,7 @@
 function [] = add_mean_waveform_pred_col(blind_pass_table,config)
 mean_waveform_pred_struct = importdata(config.FP_TO_PREDICT_ACC_CAT_USING_MEAN_WAVEFORM_NN);
 mean_wave_pred_nn = mean_waveform_pred_struct.net;
-sliced_bp_table = slice_table_for_parallel_processing(blind_pass_table);
+sliced_bp_table = slice_table_for_parallel_processing(blind_pass_table,[]);
 acc_cat_prediction = nan(size(blind_pass_table,1),1);
 parfor i=1:size(sliced_bp_table,1)
     current_data = sliced_bp_table{i};
--- examples/example_10_100.m
+++ examples/example_10_100.m
@@ -27,7 +27,9 @@ parpool('local_scratch', 37);
 disp("Finished Downloading Data");
 %% Step 4: run the blind pass with a various min_z_score (cut threshold) 
 very_beginning_time = tic;
-[blind_pass_table,fp_to_bp_table] = run_entire_clustering_algorithm_ver_2(config);
+%[blind_pass_table,fp_to_bp_table] = run_entire_clustering_algorithm_ver_2(config);
+fp_to_bp_table = "/scratch/lddavila/clustering_neuron_spikes_with_deep_learning/Default_Results_Dir/10_100/blind_pass_table/blind_pass_table.mat";
+blind_pass_table = importdata(fp_to_bp_table);
 end_time = toc(very_beginning_time);
 fprintf("Finished running blind pass it took %f seconds",end_time)
 %% Step 5: select the neurons from the blind pass
