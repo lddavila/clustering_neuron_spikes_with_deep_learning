3bb817d8aae4f552d89cb95cf6bd10dc829239fd
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-07-28 16:41:35 -0600

    set pool job

--- Utility_Functions/postprocessing/test_check_cluster_groups_for_consistent_overlap.m
+++ Utility_Functions/postprocessing/test_check_cluster_groups_for_consistent_overlap.m
@@ -11,6 +11,14 @@ config = spikesort_config();
 cd(home_dir);
 
 expanded_cluster_groups = check_cluster_groups_for_consistent_overlap(cluster_groups,config);
+config.RECORDING_NAME = "10_100";
+config.BLIND_PASS_DIR_PRECOMPUTED = fullfile(config.BLIND_PASS_DIR_PRECOMPUTED,config.RECORDING_NAME);
+
+c = parcluster('local');
+c.JobStorageLocation = config.BLIND_PASS_DIR_PRECOMPUTED;
+saveAsProfile(c, 'local_scratch');
+parpool('local_scratch', 37); 
+
 
 abs_path = create_a_file_if_doesnt_exist_and_return_abs_path("/scratch/lddavila/clustering_neuron_spikes_with_deep_learning/Default_Results_Dir/10_100/blind_pass_table_organized_into_same_groups_with_filter/expanded");
 disp("Finished creating save dir")
