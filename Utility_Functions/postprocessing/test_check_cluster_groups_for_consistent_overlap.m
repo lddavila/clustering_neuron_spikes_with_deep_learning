6453056dd972a341dcd2d944d28a54fc839316af
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-07-28 16:32:51 -0600

    changed ss script

--- Utility_Functions/postprocessing/test_check_cluster_groups_for_consistent_overlap.m
+++ Utility_Functions/postprocessing/test_check_cluster_groups_for_consistent_overlap.m
@@ -1,15 +1,18 @@
 function [] = test_check_cluster_groups_for_consistent_overlap()
 fp_to_cluster_groups = "/scratch/lddavila/clustering_neuron_spikes_with_deep_learning/Default_Results_Dir/10_100/blind_pass_table_organized_into_same_groups_with_filter/clusters_organized_by_same_group.mat";
 cluster_groups = importdata(fp_to_cluster_groups);
+disp("Finished Loading Cluster Groups")
 
 home_dir = cd("..");
 cd("..")
 addpath(genpath(pwd));
+disp("Finished adding path")
 config = spikesort_config();
 cd(home_dir);
+
 expanded_cluster_groups = check_cluster_groups_for_consistent_overlap(cluster_groups,config);
 
 abs_path = create_a_file_if_doesnt_exist_and_return_abs_path("/scratch/lddavila/clustering_neuron_spikes_with_deep_learning/Default_Results_Dir/10_100/blind_pass_table_organized_into_same_groups_with_filter/expanded");
-
+disp("Finished creating save dir")
 save(fullfile(abs_path,"expanded_cluster_groups.mat"), 'expanded_cluster_groups');
 end
