5e7db7e142b3785a598ab11a8e4d2ee28ba980a2
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-07-29 15:11:48 -0600

    created more verbose status messages

--- Utility_Functions/preprocessing/determine_which_blind_pass_neurons_overlap.m
+++ Utility_Functions/preprocessing/determine_which_blind_pass_neurons_overlap.m
@@ -61,7 +61,7 @@ for i=1:size(blind_pass_table,1)
             clusters_organized_by_same_group{cluster_group_counter} = [clusters_organized_by_same_group{cluster_group_counter};blind_pass_table(j,:)];
             already_merged(j) = 1;
         end
-        % print_status_iter_message("determine_which_blind_pass_neurons_overlap.m",[i,j],sum(~already_merged));
+        print_status_iter_message("determine_which_blind_pass_neurons_overlap.m",[i,j],sum(~already_merged));
 
     end
     cluster_group_counter = cluster_group_counter+1;
