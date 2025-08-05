df2d5835872c6281bfc8433baa097b6559547bd1
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-08-05 11:56:55 -0600

    fixed ss

--- Neural_Networks/NN_training_scripts/ss_frontera_acc_cat_pred_with_gr_univ_rank_and_mult_waves.sh
+++ Neural_Networks/NN_training_scripts/ss_frontera_acc_cat_pred_with_gr_univ_rank_and_mult_waves.sh
@@ -5,5 +5,5 @@
 #SBATCH -o verbose_wf_output.txt 
 #SBATCH -e verbose_wf_error.txt 
 #SBATCH -t 48:00:00
-module load matlab/R2023b
+module load matlab/2023b
 matlab -batch "acc_cat_pred_with_gr_univ_rank_and_mult_waves();exit;"
--- Neural_Networks/NN_training_scripts/ss_train_accuracy_cat_prediction_nn_with_grades_and_universal_rank.sh
+++ Neural_Networks/NN_training_scripts/ss_train_accuracy_cat_prediction_nn_with_grades_and_universal_rank.sh
@@ -3,5 +3,5 @@
 #SBATCH -p medium 
 #SBATCH -o output.txt 
 #SBATCH -e error.txt 
-module load matlab/R2024b
+module load matlab/R2025a
 matlab -batch "train_accuracy_cat_prediction_nn_with_grades_and_universal_rank();exit;"
