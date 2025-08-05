a43227296bec443d91e96f0997bdfac3227dc489
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-08-05 17:11:51 -0600

    modified ss to use differnt partition

--- Neural_Networks/NN_training_scripts/ss_frontera_acc_cat_pred_with_gr_univ_rank_and_mult_waves.sh
+++ Neural_Networks/NN_training_scripts/ss_frontera_acc_cat_pred_with_gr_univ_rank_and_mult_waves.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 #SBATCH -N 1
-#SBATCH -p flex
+#SBATCH -p normal
 #SBATCH -J q_learning_agent
 #SBATCH -o verbose_wf_output.txt 
 #SBATCH -e verbose_wf_error.txt 
-#SBATCH -t 48:00:00
+#SBATCH -t 4:00:00
 module load matlab/2023b
 matlab -batch "acc_cat_pred_with_gr_univ_rank_and_mult_waves();exit;"
