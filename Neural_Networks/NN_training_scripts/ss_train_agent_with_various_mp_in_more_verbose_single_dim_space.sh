9fed1bf4871fce67feba035e2bdd365418191bfd
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-08-04 18:42:52 -0600

    fixed submit script

--- Neural_Networks/NN_training_scripts/ss_train_agent_with_various_mp_in_more_verbose_single_dim_space.sh
+++ Neural_Networks/NN_training_scripts/ss_train_agent_with_various_mp_in_more_verbose_single_dim_space.sh
@@ -1,7 +1,9 @@
 #!/bin/bash 
-#SBATCH -n 40 
-#SBATCH -p medium 
+#SBATCH -N 513
+#SBATCH -p large**
+#SBATCH -J q_learning_agent
 #SBATCH -o output.txt 
 #SBATCH -e error.txt 
+#SBATCH -t 48:00:00
 module load matlab/R2023b
 matlab -batch "train_agent_with_various_mp_in_more_verbose_single_dim_space();exit;"
