d34bd5e8b3891fc3698b6b1612bfa90dc12b2555
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-08-05 17:20:06 -0600

    fixed job name in ss

--- Neural_Networks/NN_training_scripts/ss_frontera_acc_cat_pred_with_gr_univ_rank_and_mult_waves.sh
+++ Neural_Networks/NN_training_scripts/ss_frontera_acc_cat_pred_with_gr_univ_rank_and_mult_waves.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 #SBATCH -N 1
 #SBATCH -p normal
-#SBATCH -J q_learning_agent
+#SBATCH -J acc_cat_pred_with_gr_univ_rank_and_mult_waves
 #SBATCH -o verbose_wf_output.txt 
 #SBATCH -e verbose_wf_error.txt 
 #SBATCH -t 4:00:00
