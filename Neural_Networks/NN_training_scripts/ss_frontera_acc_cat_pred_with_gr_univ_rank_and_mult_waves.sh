4ea00a80c142ea90e5af2da60d26a71e572652e2
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-08-05 11:52:00 -0600

    added frontera submit script for verbose waveforms nn

---
+++ Neural_Networks/NN_training_scripts/ss_frontera_acc_cat_pred_with_gr_univ_rank_and_mult_waves.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+#SBATCH -N 1
+#SBATCH -p flex
+#SBATCH -J q_learning_agent
+#SBATCH -o verbose_wf_output.txt 
+#SBATCH -e verbose_wf_error.txt 
+#SBATCH -t 48:00:00
+module load matlab/R2023b
+matlab -batch "acc_cat_pred_with_gr_univ_rank_and_mult_waves();exit;"
