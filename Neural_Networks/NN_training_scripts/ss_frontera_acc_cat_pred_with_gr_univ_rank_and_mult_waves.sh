587b00acfc16729b3b3b327c665a305a7502e09a
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-08-06 19:24:34 -0600

    changed which queue to submit to

--- Neural_Networks/NN_training_scripts/ss_frontera_acc_cat_pred_with_gr_univ_rank_and_mult_waves.sh
+++ Neural_Networks/NN_training_scripts/ss_frontera_acc_cat_pred_with_gr_univ_rank_and_mult_waves.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 #SBATCH -N 1
-#SBATCH -p normal
+#SBATCH -p development
 #SBATCH -J acc_cat_pred_with_gr_univ_rank_and_mult_waves
 #SBATCH -o verbose_wf_output.txt 
 #SBATCH -e verbose_wf_output.txt 
-#SBATCH -t 8:00:00
+#SBATCH -t 2:00:00
 module load matlab/2023b
 matlab -batch "acc_cat_pred_with_gr_univ_rank_and_mult_waves();exit;"
