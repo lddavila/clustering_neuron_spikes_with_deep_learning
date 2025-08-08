9a1c68391da4dc359b302fae87f2c33fd580a245
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-08-07 18:15:44 -0600

    added submit script to train

---
+++ neural_network_training_scripts/ss_train_general_acc_cat_predictor_on_general_clusters.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+#SBATCH -N 1
+#SBATCH -p normal
+#SBATCH -J q_learning_agent
+#SBATCH -o output.txt 
+#SBATCH -e output.txt 
+#SBATCH -t 4:00:00
+module load matlab/2023b
+matlab -batch "train_general_acc_cat_predictor_on_general_clusters();exit;"
