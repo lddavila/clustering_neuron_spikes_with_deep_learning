587cca8dfb063b7029caa2f194a3825a67c1de24
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-08-10 17:35:36 -0600

    fixed job name in ss

--- Neural_Networks/NN_training_scripts/ss_frontera_super_nn.sh
+++ Neural_Networks/NN_training_scripts/ss_frontera_super_nn.sh
@@ -2,8 +2,8 @@
 #SBATCH -N 1
 #SBATCH -p normal
 #SBATCH -J acc_cat_pred_with_gr_univ_rank_and_mult_waves
-#SBATCH -o verbose_wf_output.txt 
-#SBATCH -e verbose_wf_output.txt 
-#SBATCH -t 2:00:00
+#SBATCH -o super_output.txt 
+#SBATCH -e super_output.txt 
+#SBATCH -t 48:00:00
 module load matlab/2023b
 matlab -batch "acc_cat_pred_with_gr_univ_rank_and_mult_waves();exit;"
