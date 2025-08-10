6cebefdd5e13cb198c4f4cb7277cf26ec9759b33
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-08-10 17:33:48 -0600

    added frontera ss

---
+++ Neural_Networks/NN_training_scripts/ss_frontera_super_nn.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+#SBATCH -N 1
+#SBATCH -p normal
+#SBATCH -J acc_cat_pred_with_gr_univ_rank_and_mult_waves
+#SBATCH -o verbose_wf_output.txt 
+#SBATCH -e verbose_wf_output.txt 
+#SBATCH -t 2:00:00
+module load matlab/2023b
+matlab -batch "acc_cat_pred_with_gr_univ_rank_and_mult_waves();exit;"
