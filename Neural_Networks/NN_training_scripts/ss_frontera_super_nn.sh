ef4fde63aa3cbac8333c2c47ab4500b778b9007d
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-08-10 18:21:42 -0600

    added ss for local cluster

--- Neural_Networks/NN_training_scripts/ss_frontera_super_nn.sh
+++ Neural_Networks/NN_training_scripts/ss_frontera_super_nn.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 #SBATCH -N 1
 #SBATCH -p normal
-#SBATCH -J acc_cat_pred_with_gr_univ_rank_and_mult_waves
+#SBATCH -J train_super_nn
 #SBATCH -o super_output.txt 
 #SBATCH -e super_output.txt 
 #SBATCH -t 48:00:00
 module load matlab/2023b
-matlab -batch "acc_cat_pred_with_gr_univ_rank_and_mult_waves();exit;"
+matlab -batch "train_super_nn();exit;"
