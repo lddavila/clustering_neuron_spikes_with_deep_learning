a2557097a17822ced404a17007e6103f50798da8
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-08-12 16:57:20 -0600

    added test to see if parallel and non parallel produce same result

---
+++ Utility_Functions/postprocessing/test_parallel_vs_non_parallel.m
@@ -0,0 +1,37 @@
+function [] = test_parallel_vs_non_parallel()
+
+home_dir = cd("..");
+cd("..");
+addpath(genpath(pwd));
+cd(home_dir);
+
+config = spikesort_config();
+blind_pass_table = importdata(config.FP_TO_TABLE_OF_ALL_BP_CLUSTERS);
+
+dir_to_save_results_to = create_a_file_if_it_doesnt_exist_and_ret_abs_path(fullfile(config.parent_save_dir,"parallel_vs_not"));
+
+cd(dir_to_save_results_to)
+all_files_in_dir = struct2table(dir(fullfile(pwd,"*.mat")));
+all_files_in_dir = string(all_files_in_dir{:,"name"});
+%run non-parallel merge
+if contains(all_files_in_dir,"results_of_non_parallel_grouping.mat")
+    results_of_non_parallel_grouping = importdata(fullfile(dir_to_save_results_to,"results_of_non_parallel_grouping.mat"));
+else
+    results_of_non_parallel_grouping = determine_which_blind_pass_neurons_overlap(blind_pass_table);
+    save("results_of_non_parallel_grouping.mat","results_of_non_parallel_grouping")
+end
+
+%run parallel merge
+if contains(all_files_in_dir,"results_of_parallel_grouping.mat")
+    results_of_parallel_grouping = importdata(fullfile(dir_to_save_results_to,"results_of_parallel_grouping.mat"));
+else
+    results_of_parallel_grouping = determine_which_blind_pass_neurons_overlap_parallel(blind_pass_table,config);
+    save("results_of_parallel_grouping.mat","results_of_parallel_grouping");
+end
+
+%now compare them to ensure that the groups are the same
+
+
+cd(home_dir);
+
+end
