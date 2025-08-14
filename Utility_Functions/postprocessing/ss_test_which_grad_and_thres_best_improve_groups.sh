19ef7e864bf564339c627fc0cb3f4287dc269af5
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-08-14 16:36:29 -0600

    fixed typo in ss

--- Utility_Functions/postprocessing/ss_test_which_grad_and_thres_best_improve_groups.sh
+++ Utility_Functions/postprocessing/ss_test_which_grad_and_thres_best_improve_groups.sh
@@ -4,4 +4,4 @@
 #SBATCH -o output.txt 
 #SBATCH -e error.txt 
 module load matlab/R2024b
-matlab -batch "test_which_grad_and_thres_best_improve_groups);exit;"
+matlab -batch "test_which_grad_and_thres_best_improve_groups();exit;"
