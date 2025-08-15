f6ed3431f41e8d772dfdef71110ceb23ed08838c
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-08-15 15:06:58 -0600

    changed ss to have same output and error file

--- Utility_Functions/postprocessing/ss_test_which_grad_and_thres_best_improve_groups.sh
+++ Utility_Functions/postprocessing/ss_test_which_grad_and_thres_best_improve_groups.sh
@@ -2,6 +2,6 @@
 #SBATCH -n 40 
 #SBATCH -p medium 
 #SBATCH -o output.txt 
-#SBATCH -e error.txt 
+#SBATCH -e output.txt 
 module load matlab/R2024b
 matlab -batch "test_which_grad_and_thres_best_improve_groups();exit;"
