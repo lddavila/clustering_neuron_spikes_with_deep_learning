79b93bd8c555b0975f465714f3a38ede4345ea17
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-08-15 16:42:54 -0600

    fixed misplaced space in ss

--- Neural_Networks/NN_training_scripts/ss_train_complex_choose_better_with_under_unit_feature.sh
+++ Neural_Networks/NN_training_scripts/ss_train_complex_choose_better_with_under_unit_feature.sh
@@ -2,6 +2,6 @@
 #SBATCH -n 40 
 #SBATCH -p medium 
 #SBATCH -o output.txt 
-#SBATCH -e output .txt 
+#SBATCH -e output.txt 
 module load matlab/R2024b
 matlab -batch "train_complex_choose_better_with_under_unit_feature();exit;"
