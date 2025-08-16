e05c849bdd467ee4637e8005ce11ef4cca3cff92
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-08-15 21:02:13 -0600

    removed the line that uses tvals unnecessarily

--- Grading_scripts/get_bin_counts_per_rep_wire.m
+++ Grading_scripts/get_bin_counts_per_rep_wire.m
@@ -8,7 +8,6 @@ for i=1:num_channels
     n = histc(max_wire, poss_wires);
     [~, max_n] = max(n);
     compare_wire = poss_wires(max_n);
-    wire_thresh = tvals(compare_wire);
     compare_peaks = peaks(compare_wire, :);
     [bin_counts{i},~] = histcounts(compare_peaks,'BinEdges',[-200:1:199,200]);
     peaks(compare_wire,:) = nan;
