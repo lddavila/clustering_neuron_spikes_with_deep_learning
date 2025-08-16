78ec313596eb13d02264580c87d50333bfee2b66
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-08-15 19:18:50 -0600

    added check index struct when needed

--- Utility_Functions/preprocessing/detect_spikes_ver_2.m
+++ Utility_Functions/preprocessing/detect_spikes_ver_2.m
@@ -3,11 +3,14 @@ spikes_matrix_unmapped = cell(1,size(ordered_list_of_channels,2));
 spikes_matrix = cell(1,config.max_channel_number);
 num_iterations = size(ordered_list_of_channels,2);
 % status_file = fopen(config.FP_TO_STATUS_FILE,"a");
-for i=1:length(ordered_list_of_channels)
+parfor i=1:length(ordered_list_of_channels)
     current_channel = ordered_list_of_channels(i);
     channel_data = importdata(fullfile(dir_with_channel_recordings,current_channel+".mat"));
     channel_data = channel_data * scale_factor;
     z_score_data = importdata(fullfile(dir_with_z_scores,current_channel+".mat"));
+    if class(z_score_data) == "struct"
+        z_score_data = z_score_data.channel_wize_z_score_data;
+    end
 
     channel_data(abs(z_score_data) < min_z_score) = 0;
 
