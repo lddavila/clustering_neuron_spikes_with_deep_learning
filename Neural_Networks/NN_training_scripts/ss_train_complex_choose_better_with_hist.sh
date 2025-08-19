67552ba65165e9f8afca14501512f3058479c4be
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-08-18 18:12:14 -0600

    changed ss error output

--- Neural_Networks/NN_training_scripts/ss_train_complex_choose_better_with_hist.sh
+++ Neural_Networks/NN_training_scripts/ss_train_complex_choose_better_with_hist.sh
@@ -2,6 +2,6 @@
 #SBATCH -n 40 
 #SBATCH -p medium 
 #SBATCH -o output.txt 
-#SBATCH -e error.txt 
+#SBATCH -e output.txt 
 module load matlab/R2024b
 matlab -batch "train_complex_choose_better_with_hist();exit;"
