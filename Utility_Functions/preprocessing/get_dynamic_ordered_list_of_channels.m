63f7e29025b01446308f98293ca51cbae927fb89
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-08-21 12:50:00 -0600

    fixed improper return

--- Utility_Functions/preprocessing/get_dynamic_ordered_list_of_channels.m
+++ Utility_Functions/preprocessing/get_dynamic_ordered_list_of_channels.m
@@ -1,9 +1,7 @@
 function [ordered_list_of_channels] = get_dynamic_ordered_list_of_channels(config)
 %get list of all available channels
 list_of_files_in_channel_dir = struct2table(dir(config.DIR_WITH_OG_CHANNEL_RECORDINGS));
-list_of_available_channels = string(list_of_files_in_channel_dir{contains(string(list_of_files_in_channel_dir{:,"name"}),".mat"),"name"});
-ordered_list_of_channels = repelem("",1,config.max_channel_number);
-for i=1:config.max_channel_number
-    ordered_list_of_channels(i) = sprintf('c%d', i);
-end
+ordered_list_of_channels = string(list_of_files_in_channel_dir{contains(string(list_of_files_in_channel_dir{:,"name"}),".mat"),"name"});
+
+
 end
