aec767c8064cc6269e29d6e7142914941f252f5b
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-08-22 18:37:43 -0600

    fixed improper fp

--- Utility_Functions/postprocessing/test_add_percentile_col.m
+++ Utility_Functions/postprocessing/test_add_percentile_col.m
@@ -4,7 +4,7 @@ cd("..")
 addpath(genpath(pwd));
 cd(home_dir);
 config = spikesort_config();
-cluster_groups =importdata(fullfile(config,"Data","results_of_parallel_grouping.mat"));
+cluster_groups =importdata(fullfile(config.base_file_path,"Data","results_of_parallel_grouping.mat"));
 cluster_groups_with_percentile = add_percentile_col(cluster_groups,config);
 
 dir_to_save = create_a_file_if_it_doesnt_exist_and_ret_abs_path(fullfile(config.parent_save_dir,"testing_feature"));
