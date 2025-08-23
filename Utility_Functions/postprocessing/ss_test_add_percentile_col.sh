9d39f30d554a3364db478527f8e2630aa6a28828
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-08-22 18:35:33 -0600

    added submit script

---
+++ Utility_Functions/postprocessing/ss_test_add_percentile_col.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+#SBATCH -n 40
+#SBATCH -p medium
+#SBATCH -o output.txt 
+#SBATCH -e output.txt 
+#SBATCH -N 1
+module load matlab/R2024b
+matlab -batch "test_add_percentile_col; exit;"
