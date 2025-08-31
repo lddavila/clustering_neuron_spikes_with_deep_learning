aaa9e17fa336d3e68951d67db4c86d9be5390019
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-08-31 17:16:11 -0600

    added missing backslash

--- test_other_spike_sorters/unpack_no_drift_sim.py
+++ test_other_spike_sorters/unpack_no_drift_sim.py
@@ -1,4 +1,4 @@
 import os
 from extract_recordings import extract_recordings 
-fp_to_no_drift_sim = os.path.join("scratch","afriedman","clustering_neuron_spikes_with_deep_learning","Data","sim_no_drift")
+fp_to_no_drift_sim = os.path.join("/scratch","afriedman","clustering_neuron_spikes_with_deep_learning","Data","sim_no_drift")
 extract_recordings(fp_to_no_drift_sim)
