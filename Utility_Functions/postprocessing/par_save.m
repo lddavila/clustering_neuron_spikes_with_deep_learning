e33b8dd007f97abfc4e14aa9046aef9395903ddf
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-09-01 17:33:49 -0600

    fixed missing var

--- Utility_Functions/postprocessing/par_save.m
+++ Utility_Functions/postprocessing/par_save.m
@@ -3,6 +3,7 @@ file_save_name_as_char = char(file_save_name);
 if ~strcmp(file_save_name_as_char(end-3:end), '.mat') %if it's missing the .mat file extension add it yourself
     file_save_name = file_save_name+".mat";
 end
+twoGB_in_bytes = 2 * 1024^3; % 2 * (1024 * 1024 * 1024) %used to check for larger files as expected in real simulations
 data_to_save_data = whos("data_to_save");
 %check if the file is too large to save in normal format
 if data_to_save_data.bytes >= twoGB_in_bytes
