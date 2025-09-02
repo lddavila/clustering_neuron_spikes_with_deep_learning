4955c34150399b38d9823fae0b9592b105f3190d
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-09-02 17:51:13 -0600

    removed redundant check

--- Utility_Functions/calling_main_clustering_algorithm/run_entire_clustering_algorithm_ver_2.m
+++ Utility_Functions/calling_main_clustering_algorithm/run_entire_clustering_algorithm_ver_2.m
@@ -15,8 +15,8 @@ precomputed_dir = config.BLIND_PASS_DIR_PRECOMPUTED;
 disp("Finished setting the blind pass directory")
 
 %step 3: get list of existing files in the precomputed dir
-        %doing this enables us to tell which parts of the algorithm can be
-        %skipped as to avoid repetition
+%doing this enables us to tell which parts of the algorithm can be
+%skipped as to avoid repetition
 list_of_existing_files =struct2table(dir(fullfile(precomputed_dir,"**",".")));
 rows_to_exclude = string(list_of_existing_files{:,"name"}) == "." | string(list_of_existing_files{:,"name"})=="..";
 list_of_existing_files(rows_to_exclude,:) = [];
@@ -77,7 +77,7 @@ if ~ismember(fullfile(precomputed_dir,"blind_pass.txt"),what_is_computed)
         beginning_time = tic;
         spike_windows_dir = create_a_file_if_it_doesnt_exist_and_ret_abs_path(fullfile(precomputed_dir,"spike_windows min_z_score " + string(min_z_score) + " num dps "+ string(num_dps)));
         spike_windows_fp = get_spike_windows_ver_2(ordered_list_of_channels,fp_to_spikes_per_channel,min_z_score,num_dps,z_score_dir,spike_windows_dir,config);
-       
+
         end_time = toc(beginning_time);
         fprintf("Finished getting spike windows for z score %f, it took %f seconds\n",min_z_score,end_time);
 
@@ -85,43 +85,39 @@ if ~ismember(fullfile(precomputed_dir,"blind_pass.txt"),what_is_computed)
         beginning_time = tic;
         dictionaries_dir = create_a_file_if_it_doesnt_exist_and_ret_abs_path(fullfile(precomputed_dir,"dictionaries min_z_score "+string(min_z_score)+ " num_dps "+string(num_dps)));
         % step 9d: get maps of each tetrode to its spikes
-        if 1
-            % clc;
-            get_dictionaries_of_all_spikes_ver_3(art_tetr_array,spike_windows_fp,dir_with_channel_recordings,timestamps,num_dps,scale_factor,dictionaries_dir,config);
-            %tetrode_dictionary
-            %keys: "t" + tetrode number
-            %values: all channels which are part of the current dictionary
-            %spike_tetrode_dictionary
-            %keys: "t" + tetrode number
-            %values: the spikes for the current tetrode organized as follows
-            %[numwires, numspikes, numdp] = size(raw);
-            %numwires: number of channels
-            %numspikes: number of spikes
-            %numdp: number of datapoints
-            %timing_tetrode_dictionary
-            %channel_to_tetrode_dictionary
-            %keys: "c" + channel number
-            %values: tetrode which the current channel belongs to
-            %spiking_channel_tetrode_dictionary
-            %keys: "t"+ tetrode number
-            %values: a list of which channel was the actual spiking channel, ordered in the same way as spike_tetrode_dictionary
-            %spike_tetrode_dictionary_samples_format
-            %keys: "t"+tetrode number
-            %values: the spikes for the current tetrode organzied as follows
-            %[numdp, numspikes, numswires] = size(raw);
-            %numwires: number of channels
-            %numspikes: number of spikes
-            %numdp: number of datapoints
-            %timing_tetrode_dictionary
-            % number_of_non_empty_tetrodes = check_how_many_tetrodes_have_more_than_zero_spikes(spike_tetrode_dictionary);
-            % disp("Non Empty Tetrodes:" + string(number_of_non_empty_tetrodes))
-            % clc;
 
-        else
-            disp("dictionaries min_z_score " + string(min_z_score) + " num_dps " + string(num_dps) +" has been detected and will not be recreated.");
-            disp("To recalculate delete the original or change your precomputed directory.")
+        % clc;
+        get_dictionaries_of_all_spikes_ver_3(art_tetr_array,spike_windows_fp,dir_with_channel_recordings,timestamps,num_dps,scale_factor,dictionaries_dir,config);
+        %tetrode_dictionary
+        %keys: "t" + tetrode number
+        %values: all channels which are part of the current dictionary
+        %spike_tetrode_dictionary
+        %keys: "t" + tetrode number
+        %values: the spikes for the current tetrode organized as follows
+        %[numwires, numspikes, numdp] = size(raw);
+        %numwires: number of channels
+        %numspikes: number of spikes
+        %numdp: number of datapoints
+        %timing_tetrode_dictionary
+        %channel_to_tetrode_dictionary
+        %keys: "c" + channel number
+        %values: tetrode which the current channel belongs to
+        %spiking_channel_tetrode_dictionary
+        %keys: "t"+ tetrode number
+        %values: a list of which channel was the actual spiking channel, ordered in the same way as spike_tetrode_dictionary
+        %spike_tetrode_dictionary_samples_format
+        %keys: "t"+tetrode number
+        %values: the spikes for the current tetrode organzied as follows
+        %[numdp, numspikes, numswires] = size(raw);
+        %numwires: number of channels
+        %numspikes: number of spikes
+        %numdp: number of datapoints
+        %timing_tetrode_dictionary
+        % number_of_non_empty_tetrodes = check_how_many_tetrodes_have_more_than_zero_spikes(spike_tetrode_dictionary);
+        % disp("Non Empty Tetrodes:" + string(number_of_non_empty_tetrodes))
+        % clc;
 
-        end
+
         end_time = toc(beginning_time);
         fprintf('Getting dictionaries took: %f\n',end_time)
 
