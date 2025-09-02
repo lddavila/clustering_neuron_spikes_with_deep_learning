a9b6b38ac873d129c80c7f2e5f52c1ad41e5a845
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-09-02 16:59:47 -0600

    modified to use spike windows save structure to enable the use of lazy loading

--- Utility_Functions/calling_main_clustering_algorithm/run_entire_clustering_algorithm_ver_2.m
+++ Utility_Functions/calling_main_clustering_algorithm/run_entire_clustering_algorithm_ver_2.m
@@ -76,26 +76,8 @@ if ~ismember(fullfile(precomputed_dir,"blind_pass.txt"),what_is_computed)
         % step 9c; Get all the data points from the potential spikes
         beginning_time = tic;
         spike_windows_dir = create_a_file_if_it_doesnt_exist_and_ret_abs_path(fullfile(precomputed_dir,"spike_windows min_z_score " + string(min_z_score) + " num dps "+ string(num_dps)));
-        if ~ismember(fullfile(spike_windows_dir,"spike_windows.mat"),what_is_computed)
-            spike_windows = get_spike_windows_ver_2(ordered_list_of_channels,fp_to_spikes_per_channel,min_z_score,num_dps,z_score_dir,config);
-            spike_windows_data = whos("spike_windows");
-            %each array is made up of 4 numbers:
-            %the first is the beginning of the spike window
-            %the second is the end of the spike_window
-            %the third is the original channel of the spike
-            %the fourth is the original the peak of the spike according to find_peaks'
-            if spike_windows_data.bytes >= twoGB_in_bytes
-                save(fullfile(spike_windows_dir,"spike_windows.mat"),"spike_windows", '-v7.3', '-nocompression');
-            else
-                save(fullfile(spike_windows_dir,"spike_windows.mat"),"spike_windows");
-            end
-        else
-            disp("spike_windows min_z_score " + string(min_z_score) + " num dps " + string(num_dps) + " has been detected and will be skipped.")
-            disp("To recalculate delete the original or change your precomputed directory.")
-            disp("Beginning loading spike windows");
-            spike_windows = arrayDatastore(importdata(fullfile(precomputed_dir,"spike_windows min_z_score "+string(min_z_score)+" num dps "+string(num_dps),"spike_windows.mat")));
-            disp("Finished Loading Spike windows");
-        end
+        spike_windows_fp = get_spike_windows_ver_2(ordered_list_of_channels,fp_to_spikes_per_channel,min_z_score,num_dps,z_score_dir,spike_windows_dir,config);
+       
         end_time = toc(beginning_time);
         fprintf("Finished getting spike windows for z score %f, it took %f seconds\n",min_z_score,end_time);
 
--- Utility_Functions/preprocessing/get_spike_windows_ver_2.m
+++ Utility_Functions/preprocessing/get_spike_windows_ver_2.m
@@ -1,54 +1,67 @@
-function [spike_windows] = get_spike_windows_ver_2(ordered_list_of_channels,fp_to_spikes_per_channel,desired_z_score,desired_number_of_data_points,dir_with_channel_z_scores,config)
+function [spike_windows_fp] = get_spike_windows_ver_2(ordered_list_of_channels,fp_to_spikes_per_channel,desired_z_score,desired_number_of_data_points,dir_with_channel_z_scores,spike_windows_dir,config)
+spike_windows_fp =fullfile(spike_windows_dir,"spike_windows.mat") ;
+if ~ismember(fullfile(spike_windows_dir,"spike_windows.mat"),config.ALREADY_DONE_FILES)
+    %each array is made up of 4 numbers:
+    %the first is the beginning of the spike window
+    %the second is the end of the spike_window
+    %the third is the original channel of the spike
+    %the fourth is the original the peak of the spike according to find_peaks'
+    number_of_iterations = length(ordered_list_of_channels);
+    channel_numbers = strrep(ordered_list_of_channels,"c","");
+    channel_numbers =strrep(channel_numbers,".mat","");
+    channel_numbers = str2double(channel_numbers);
+    % status_file = config.FP_TO_STATUS_FILE;
+    q = parallel.pool.DataQueue;
+    afterEach(q,@print_message_using_dataqueue)
+    print_message_using_dataqueue(number_of_iterations,"get_spike_windows_ver_2.m")
 
-number_of_iterations = length(ordered_list_of_channels);
-channel_numbers = strrep(ordered_list_of_channels,"c","");
-channel_numbers =strrep(channel_numbers,".mat","");
-channel_numbers = str2double(channel_numbers);
-% status_file = config.FP_TO_STATUS_FILE;
-q = parallel.pool.DataQueue;
-afterEach(q,@print_message_using_dataqueue)
-print_message_using_dataqueue(number_of_iterations,"get_spike_windows_ver_2.m")
-
-spikes_per_channel_ds = matfile(fp_to_spikes_per_channel);
-for i=1:length(ordered_list_of_channels)
-    current_channel = ordered_list_of_channels(i);
-    spike_windows_unmapped = cell(1,config.max_channel_number);
-    channel_wise_z_score = importdata(fullfile(dir_with_channel_z_scores,current_channel));
-    tmp = spikes_per_channel_ds.data_to_save(1, channel_numbers(i));
-    tmp = tmp{1};
-    for j=1:length(tmp)
-        channel_i_peak_j = tmp(j); %get the current peak
-        channel_i_peak_j_z_score = channel_wise_z_score(channel_i_peak_j); %get the z_score for current spike
-        if channel_i_peak_j < fix(desired_number_of_data_points/2) || channel_i_peak_j+fix(desired_number_of_data_points/2) > length(channel_wise_z_score) %round towards zero
-            %if peak is too early or too late don't use it
-            spike_windows_unmapped{i}{j} = int32([0,0,0,0]); %zero is used as a flag since they should never be 0 as it is an invalid matlab index
-        else
-            if abs(channel_i_peak_j_z_score) >= desired_z_score 
-                if channel_i_peak_j - fix(desired_number_of_data_points/2) ~= 0 && channel_i_peak_j + fix(desired_number_of_data_points/2) <= length(channel_wise_z_score)
-                    spike_windows_unmapped{i}{j} = int32([channel_i_peak_j - fix(desired_number_of_data_points/2),...
-                        channel_i_peak_j + fix(desired_number_of_data_points/2),...
-                        i,...
-                        channel_i_peak_j]);
-                else
-                    spike_windows_unmapped{i}{j}=int32([0,0,0,0]); 
-                end
-                %each  is made up of 4 numbers:
-                %the first is the beginning of the spike window
-                %the second is the end of the spike_window
-                %the third is the original channel of the spike
-                %the fourth is the original the peak of the spike according to 
-
+    spikes_per_channel_ds = matfile(fp_to_spikes_per_channel);
+    for i=1:length(ordered_list_of_channels)
+        current_channel = ordered_list_of_channels(i);
+        spike_windows_unmapped = cell(1,config.max_channel_number);
+        channel_wise_z_score = importdata(fullfile(dir_with_channel_z_scores,current_channel));
+        tmp = spikes_per_channel_ds.data_to_save(1, channel_numbers(i));
+        tmp = tmp{1};
+        for j=1:length(tmp)
+            channel_i_peak_j = tmp(j); %get the current peak
+            channel_i_peak_j_z_score = channel_wise_z_score(channel_i_peak_j); %get the z_score for current spike
+            if channel_i_peak_j < fix(desired_number_of_data_points/2) || channel_i_peak_j+fix(desired_number_of_data_points/2) > length(channel_wise_z_score) %round towards zero
+                %if peak is too early or too late don't use it
+                spike_windows_unmapped{i}{j} = int32([0,0,0,0]); %zero is used as a flag since they should never be 0 as it is an invalid matlab index
             else
-                spike_windows_unmapped{i}{j} = int32([0,0,0,0]);
+                if abs(channel_i_peak_j_z_score) >= desired_z_score
+                    if channel_i_peak_j - fix(desired_number_of_data_points/2) ~= 0 && channel_i_peak_j + fix(desired_number_of_data_points/2) <= length(channel_wise_z_score)
+                        spike_windows_unmapped{i}{j} = int32([channel_i_peak_j - fix(desired_number_of_data_points/2),...
+                            channel_i_peak_j + fix(desired_number_of_data_points/2),...
+                            i,...
+                            channel_i_peak_j]);
+                    else
+                        spike_windows_unmapped{i}{j}=int32([0,0,0,0]);
+                    end
+                    %each  is made up of 4 numbers:
+                    %the first is the beginning of the spike window
+                    %the second is the end of the spike_window
+                    %the third is the original channel of the spike
+                    %the fourth is the original the peak of the spike according to
+
+                else
+                    spike_windows_unmapped{i}{j} = int32([0,0,0,0]);
+                end
             end
+
         end
-        
+        send(q,[]);
     end
-    send(q,[]);
+
+    spike_windows = cell(1,config.max_channel_number);
+    for i=1:length(channel_numbers)
+        spike_windows{channel_numbers(i)} = spike_windows_unmapped{i};
+    end
+    par_save(spike_windows_fp,spike_windows)
+else
+    disp("spike_windows min_z_score " + string(min_z_score) + " num dps " + string(num_dps) + " has been detected and will be skipped.")
+    disp("To recalculate delete the original or change your precomputed directory.")
 end
 
-spike_windows = cell(1,config.max_channel_number);
-for i=1:length(channel_numbers)
-    spike_windows{channel_numbers(i)} = spike_windows_unmapped{i};
-end
+
 end
