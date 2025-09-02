38d911f827e71f35643d063ee78402dcf0940e39
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-09-02 16:25:02 -0600

    changed spikes per channel to save style to enable datastore reading later

--- Utility_Functions/calling_main_clustering_algorithm/run_entire_clustering_algorithm_ver_2.m
+++ Utility_Functions/calling_main_clustering_algorithm/run_entire_clustering_algorithm_ver_2.m
@@ -68,19 +68,7 @@ if ~ismember(fullfile(precomputed_dir,"blind_pass.txt"),what_is_computed)
         beginning_time = tic;
         % step 9b: get potential spikes from continuous recordings
         spikes_per_chan_dir = create_a_file_if_it_doesnt_exist_and_ret_abs_path(fullfile(precomputed_dir,"spikes_per_channel min_z_score "+string(min_z_score)));
-        if ~ismember(fullfile(spikes_per_chan_dir,"spikes_per_channel.mat"),what_is_computed)
-            spikes_per_channel = detect_spikes_ver_2(ordered_list_of_channels,dir_with_channel_recordings,z_score_dir,min_z_score,scale_factor,config);
-            spikes_per_channel_info = whos('spikes_per_channel');
-            if spikes_per_channel_info.bytes >= twoGB_in_bytes
-                save(fullfile(spikes_per_chan_dir,"spikes_per_channel.mat"), "spikes_per_channel", '-v7.3', '-nocompression');
-            else
-                save(fullfile(spikes_per_chan_dir,"spikes_per_channel.mat"),"spikes_per_channel");
-            end
-        else
-            disp("spikes_per_channel min_z_score "+ string(min_z_score)+ " has been detected and will be skipped.")
-            disp("To recalculate delete the original or change your precomputed directory.")
-            load(fullfile(precomputed_dir,"spikes_per_channel min_z_score " + string(min_z_score),"spikes_per_channel.mat"), "spikes_per_channel")
-        end
+        spikes_per_channel = detect_spikes_ver_2(spikes_per_channel_dir,ordered_list_of_channels,dir_with_channel_recordings,z_score_dir,min_z_score,scale_factor,config);
         end_time = toc(beginning_time);
         fprintf("Finished cutting spikes per channel for z score %f, it took %f seconds\n",min_z_score,end_time);
 
--- Utility_Functions/preprocessing/detect_spikes_ver_2.m
+++ Utility_Functions/preprocessing/detect_spikes_ver_2.m
@@ -1,33 +1,37 @@
-function [spikes_matrix] = detect_spikes_ver_2(ordered_list_of_channels,dir_with_channel_recordings,dir_with_z_scores,min_z_score,scale_factor,config)
-spikes_matrix_unmapped = cell(1,size(ordered_list_of_channels,2));
-spikes_matrix = cell(1,config.max_channel_number);
-num_iterations = size(ordered_list_of_channels,2);
-% status_file = fopen(config.FP_TO_STATUS_FILE,"a");
-q = parallel.pool.DataQueue;
-afterEach(q,@print_message_using_dataqueue)
-num_iterations = length(ordered_list_of_channels);
-print_message_using_dataqueue(num_iterations,"detect_spikes_ver_2.m")
-parfor i=1:length(ordered_list_of_channels)
-    current_channel = ordered_list_of_channels(i);
-    channel_data = importdata(fullfile(dir_with_channel_recordings,current_channel));
-    channel_data = channel_data * scale_factor;
-    z_score_data = importdata(fullfile(dir_with_z_scores,current_channel));
-    if class(z_score_data) == "struct"
-        z_score_data = z_score_data.channel_wize_z_score_data;
+function [] = detect_spikes_ver_2(spikes_per_channel_dir,ordered_list_of_channels,dir_with_channel_recordings,dir_with_z_scores,min_z_score,scale_factor,config)
+
+%if the 
+if ~ismember(fullfile(spikes_per_channel_dir,"spikes_per_channel.mat"),config.ALREADY_DONE_FILES)
+    spikes_matrix_unmapped = cell(1,size(ordered_list_of_channels,2));
+    spikes_matrix = cell(1,config.max_channel_number);
+    % status_file = fopen(config.FP_TO_STATUS_FILE,"a");
+    q = parallel.pool.DataQueue;
+    afterEach(q,@print_message_using_dataqueue)
+    num_iterations = length(ordered_list_of_channels);
+    print_message_using_dataqueue(num_iterations,"detect_spikes_ver_2.m")
+    parfor i=1:length(ordered_list_of_channels)
+        current_channel = ordered_list_of_channels(i);
+        channel_data = importdata(fullfile(dir_with_channel_recordings,current_channel));
+        channel_data = channel_data * scale_factor;
+        z_score_data = importdata(fullfile(dir_with_z_scores,current_channel));
+        if class(z_score_data) == "struct"
+            z_score_data = z_score_data.channel_wize_z_score_data;
+        end
+
+        channel_data(abs(z_score_data) < min_z_score) = 0;
+
+        [~,pk_locs] = findpeaks(channel_data);
+        spikes_matrix_unmapped{i} = pk_locs;
+        send(q,[]);
     end
-
-    channel_data(abs(z_score_data) < min_z_score) = 0;
-
-    [~,pk_locs] = findpeaks(channel_data);
-    spikes_matrix_unmapped{i} = pk_locs;
-    send(q,[]);
+    channels = strrep(ordered_list_of_channels,"c","");
+    channels = strrep(channels,".mat","");
+    for i=1:length(channels)
+        spikes_matrix{str2double(channels(i))} = spikes_matrix_unmapped{i};
+    end
+    par_save(fullfile(spikes_per_channel_dir,"spikes_per_channel.mat"),spikes_matrix);
+else
+    disp("spikes_per_channel min_z_score "+ string(min_z_score)+ " has been detected and will be skipped.")
+    disp("To recalculate delete the original or change your precomputed directory.")
 end
-channels = strrep(ordered_list_of_channels,"c","");
-channels = strrep(channels,".mat","");
-for i=1:length(channels)
-    spikes_matrix{str2double(channels(i))} = spikes_matrix_unmapped{i};
-end
-% status_message = "\n"+print_status_iter_message("detect_spikes_ver_2.m",i,num_iterations);
-% fprintf(status_file,status_message);
-% fclose(status_file);
 end
