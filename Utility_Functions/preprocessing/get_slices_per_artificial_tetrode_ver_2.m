167be659327c1458954fb9ce9011d0be7903c16a
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-09-03 21:03:21 -0600

    fixed the for loop iterator

--- Utility_Functions/preprocessing/get_slices_per_artificial_tetrode_ver_2.m
+++ Utility_Functions/preprocessing/get_slices_per_artificial_tetrode_ver_2.m
@@ -70,7 +70,7 @@ parfor i=1:size(sorted_spike_windows_for_current_tetrode,1)
     current_timing_slice = timing_matrix(current_window(1,1):current_window(1,2) -1);
     time_slices(i,:) = current_timing_slice;
 
-    for j=1:simple_channel_list
+    for j=simple_channel_list
         spike_slices(j,i,:) = channels_data{j}(current_window(1,1) :current_window(i,2) -1);
         spike_slices_in_samples_format(:,j,i) = channels_data{j}(current_window(1,1) :current_window(i,2)-1);
     end
