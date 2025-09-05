a8655e71a99cf2de4af72b527fe69e53823d437c
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-09-04 18:58:52 -0600

    fixed ss

--- examples/ss_ks4_1200_unit_sim_no_drift_first_300_secs.sh
+++ examples/ss_ks4_1200_unit_sim_no_drift_first_300_secs.sh
@@ -4,4 +4,4 @@
 #SBATCH -o output.txt 
 #SBATCH -e output.txt 
 module load matlab/R2024b
-matlab -batch "ss_ks4_1200_unit_sim_no_drift_first_300_secs();exit;"
+matlab -batch "ks4_1200_unit_sim_no_drift_first_300_secs();exit;"
