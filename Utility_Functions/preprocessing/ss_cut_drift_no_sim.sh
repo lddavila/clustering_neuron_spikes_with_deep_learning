f3f027b76c150d6176af7601657fedbe7801b0da
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-09-04 18:43:34 -0600

    fixed ss

--- Utility_Functions/preprocessing/ss_cut_drift_no_sim.sh
+++ Utility_Functions/preprocessing/ss_cut_drift_no_sim.sh
@@ -4,4 +4,4 @@
 #SBATCH -o output.txt 
 #SBATCH -e output.txt 
 module load matlab/R2024b
-matlab -batch "get_first_n_seconds_of_recording('/scratch/afriedman/clustering_neuron_spikes_with_deep_learning/Data/sim_no_drift',300,'/scratch/afriedman/clustering_neuron_spikes_with_deep_learning/Data/sim_no_drift_first_300_seconds');exit;"
+matlab -batch "get_first_n_seconds_of_recording('/scratch/afriedman/clustering_neuron_spikes_with_deep_learning/Data/sim_no_drift_decompressed',300,'/scratch/afriedman/clustering_neuron_spikes_with_deep_learning/Data/sim_no_drift_first_300_seconds');exit;"
