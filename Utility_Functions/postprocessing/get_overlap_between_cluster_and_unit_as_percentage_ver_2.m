28634ee01620bc991f57349c37742e3967980696
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-09-08 14:25:34 -0600

    optimized the overlap computation

--- Utility_Functions/postprocessing/get_overlap_between_cluster_and_unit_as_percentage_ver_2.m
+++ Utility_Functions/postprocessing/get_overlap_between_cluster_and_unit_as_percentage_ver_2.m
@@ -11,14 +11,18 @@ for i=1:length(ground_truth)
     current_unit_ts_locs = ground_truth{i};
     number_of_times_current_unit_spikes = size(current_unit_ts_locs,2);
     current_unit_ts = timestamps(current_unit_ts_locs);
-    number_of_ts_in_common = 0;
-    for j=1:number_of_times_current_unit_spikes
-        diffs_between_unit_ts_and_cluster_ts = current_unit_ts(j) - timestamps_of_cluster;
-        if any(abs(diffs_between_unit_ts_and_cluster_ts) < time_delta)
-            number_of_ts_in_common = number_of_ts_in_common +1;
-        end
-
-    end
+    
+    %ORIGINAL BEGINS BELOW
+    % number_of_ts_in_common = 0;
+    % for j=1:number_of_times_current_unit_spikes
+    %     diffs_between_unit_ts_and_cluster_ts = current_unit_ts(j) - timestamps_of_cluster;
+    %     if any(abs(diffs_between_unit_ts_and_cluster_ts) < time_delta)
+    %         number_of_ts_in_common = number_of_ts_in_common +1;
+    %     end
+    % 
+    % end
+    %ORIGINAL ENDS
+    number_of_ts_in_common = ismembertol(current_unit_ts, timestamps_of_cluster, time_delta, 'DataScale', 1);
     percentage_of_units_spikes_in_cluster = (number_of_ts_in_common / number_of_times_current_unit_spikes) * 100;
     if percentage_of_units_spikes_in_cluster > max_overlap_percentage
         max_overlap_percentage = percentage_of_units_spikes_in_cluster;
