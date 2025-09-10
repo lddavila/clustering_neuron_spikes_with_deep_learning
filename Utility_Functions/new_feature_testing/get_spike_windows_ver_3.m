635657b2da63ec228e0078f5a7a051219b2f98a7
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-09-10 17:59:54 -0600

    fixed bug (hopefully)

--- Utility_Functions/new_feature_testing/get_spike_windows_ver_3.m
+++ Utility_Functions/new_feature_testing/get_spike_windows_ver_3.m
@@ -11,7 +11,7 @@ for i=1:length(channels)
     current_channel = channels(i);
     disp("file we're importing from")
     disp(fullfile(old_spike_windows_dir,current_channel))
-    previously_found_spike_windows = importdata(fullfile(old_spike_windows_dir,current_channel));
+    previously_found_spike_windows = importdata(fullfile(old_spike_windows_dir,"c"+string(current_channel)+".mat"));
     disp("Size of old spike windows")
     disp(size(previously_found_spike_windows));
     spike_windows = previously_found_spike_windows(previously_found_spike_windows(:,5)>=desired_z_score,:);
