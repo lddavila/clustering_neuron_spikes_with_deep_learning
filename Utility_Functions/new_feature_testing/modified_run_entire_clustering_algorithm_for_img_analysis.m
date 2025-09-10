65c31c1c0d6b4803f28566228dc15795bc43e4b6
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-09-10 13:11:50 -0600

    fixed delete operations

--- Utility_Functions/new_feature_testing/modified_run_entire_clustering_algorithm_for_img_analysis.m
+++ Utility_Functions/new_feature_testing/modified_run_entire_clustering_algorithm_for_img_analysis.m
@@ -166,10 +166,10 @@ for min_z_score = z_scores_to_check %this for loop is probably redundant because
 
     %do file cleanup to ensure no necessary steps are skipped due to
     %previous permutations
-    delete(fullfile(config.BLIND_PASS_DIR_PRECOMPUTED,"dictionaries min_z_score "+string(min_z_score)+" num_dps "+string(num_dps)));
-    delete(fullfile(config.BLIND_PASS_DIR_PRECOMPUTED,"initial_pass min z_score"+string(min_z_score)));
-    delete(fullfile(config.BLIND_PASS_DIR_PRECOMPUTED,"initial_pass_results min z_score "+string(min_z_score)))
-    delete(fullfile(config.BLIND_PASS_DIR_PRECOMPUTED,"spike_windows min_z_score "+string(min_z_score)+" num dps "+string(num_dps)))
+    rmdir(fullfile(config.BLIND_PASS_DIR_PRECOMPUTED,"dictionaries min_z_score "+string(min_z_score)+" num_dps "+string(num_dps)),'s');
+    rmdir(fullfile(config.BLIND_PASS_DIR_PRECOMPUTED,"initial_pass min z_score"+string(min_z_score)),'s');
+    rmdir(fullfile(config.BLIND_PASS_DIR_PRECOMPUTED,"initial_pass_results min z_score "+string(min_z_score)),'s')
+    rmdir(fullfile(config.BLIND_PASS_DIR_PRECOMPUTED,"spike_windows min_z_score "+string(min_z_score)+" num dps "+string(num_dps)),'s')
 
 
 
