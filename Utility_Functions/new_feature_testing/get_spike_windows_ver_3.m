63746cd0b7bed0d5ba12015682e6a4ae1d3ed6d8
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-09-10 17:49:40 -0600

    MORE prints

--- Utility_Functions/new_feature_testing/get_spike_windows_ver_3.m
+++ Utility_Functions/new_feature_testing/get_spike_windows_ver_3.m
@@ -7,8 +7,16 @@ function [] = get_spike_windows_ver_3(channels,desired_z_score,old_spike_windows
 disp("entered get_spike_windows_ver_3")
 for i=1:length(channels)
     current_channel = channels(i);
+    disp("file we're importing from")
+    disp(fullfile(old_spike_windows_dir,current_channel))
     previously_found_spike_windows = importdata(fullfile(old_spike_windows_dir,current_channel));
+    disp("Size of old spike windows")
+    disp(size(previously_found_spike_windows));
     spike_windows = previously_found_spike_windows(previously_found_spike_windows(:,5)>=desired_z_score,:);
+    disp("size of new spike windows");
+    disp(size(spike_windows))
+    disp("save path for spike windows")
+    disp(fullfile(new_spike_windows_dir,current_channel),spike_windows)
     par_save(fullfile(new_spike_windows_dir,current_channel),spike_windows)
 end
 disp("Finished getting spike windows")
