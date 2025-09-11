833d713ecff101d8ea2d058ea35ad1a5f12c4228
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-09-10 18:15:21 -0600

    removed prints

--- Utility_Functions/new_feature_testing/get_spike_windows_ver_3.m
+++ Utility_Functions/new_feature_testing/get_spike_windows_ver_3.m
@@ -3,25 +3,13 @@ function [] = get_spike_windows_ver_3(channels,desired_z_score,old_spike_windows
 %differs from get_spike_windows_ver_2 because instead of creating the spike windows array per channel
 %this function reads an existing spike windows z score and eliminates any
 %spikes that have a z score lower than the currently desired on
-
-disp("entered get_spike_windows_ver_3")
-disp("channels that were entered with")
-disp(channels)
 for i=1:length(channels)
     current_channel = channels(i);
-    disp("file we're importing from")
-    disp(fullfile(old_spike_windows_dir,"c"+string(current_channel)+".mat"))
     previously_found_spike_windows = importdata(fullfile(old_spike_windows_dir,"c"+string(current_channel)+".mat"));
-    disp("Size of old spike windows")
-    disp(size(previously_found_spike_windows));
     spike_windows = previously_found_spike_windows(previously_found_spike_windows(:,5)>=desired_z_score,:);
-    disp("size of new spike windows");
-    disp(size(spike_windows))
-    disp("save path for spike windows")
-    display(fullfile(new_spike_windows_dir,current_channel))
     par_save(fullfile(new_spike_windows_dir,current_channel),spike_windows)
 end
-disp("Finished getting spike windows")
+
 
 
 end
