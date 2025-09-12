92443028f4c5a7fb738e5c01496cd1e6ce963839
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-09-11 22:59:51 -0600

    removed missing ts

--- Utility_Functions/new_feature_testing/add_clusters_to_bp_table.m
+++ Utility_Functions/new_feature_testing/add_clusters_to_bp_table.m
@@ -17,7 +17,7 @@ for i=1:size(sliced_bp_table,1)
     catch
         % disp("Failed to load output file")
         % disp(current_data{1,"fp_to_output"})
-        send(q,[]);
+        % send(q,[]);
         continue;
     end
 
@@ -27,7 +27,7 @@ for i=1:size(sliced_bp_table,1)
     catch
         % disp("Failed to load timestamps of spikes");
         % disp(current_data{1,"fp_to_reg_timestamps_of_spikes"});
-        send(q,[]);
+        % send(q,[]);
         continue;
     end
     idx_b4_filt = extract_clusters_from_output(output(:,1),output);
