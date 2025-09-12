9af466ce547c3fa41b2866a3c6fc2f20f44f782c
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-09-12 14:45:50 -0600

    changed output on frontera ss

--- Utility_Functions/new_feature_testing/ss_frontera_test_finding_z_score_With_img_incrementally.sh
+++ Utility_Functions/new_feature_testing/ss_frontera_test_finding_z_score_With_img_incrementally.sh
@@ -2,8 +2,8 @@
 #SBATCH -N 2
 #SBATCH -p normal
 #SBATCH -J training
-#SBATCH -o under_unit_output.txt 
-#SBATCH -e under_unit_output_error.txt 
+#SBATCH -o output.txt
+#SBATCH -e output.txt
 #SBATCH -t 48:00:00
 module load matlab/2023b
 matlab -batch "test_finding_z_score_With_img_incrementally();exit;"
