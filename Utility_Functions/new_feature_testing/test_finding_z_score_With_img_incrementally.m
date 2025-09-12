946ee16274370f76fb3d040f096fca38d7d4708b
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-09-11 23:59:53 -0600

    added delete to eliminate old pool

--- Utility_Functions/new_feature_testing/test_finding_z_score_With_img_incrementally.m
+++ Utility_Functions/new_feature_testing/test_finding_z_score_With_img_incrementally.m
@@ -157,6 +157,7 @@ if ~ismember(fullfile(config.BLIND_PASS_DIR_PRECOMPUTED,"table_of_image_accuracy
     afterEach(q,@print_status_bar)
     print_status_bar(num_iterations,"getting accuracy for each image.m")
     cell_array_of_image_accuracy_data = cell(size(art_tetrode_array,1),1);
+    delete(gcp('nocreate'))
     parpool("threads");   
     parfor i=1:size(art_tetrode_array,1)
         sub_cell_array_of_image_accuracy_data = cell(length(increments_to_try),1);
