46128c64b7224ff27790aa6e33708c7c08eb72e2
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-09-13 23:40:06 -0600

    removed redundant parfor

--- Utility_Functions/new_feature_testing/modified_run_entire_clustering_algorithm_for_img_analysis.m
+++ Utility_Functions/new_feature_testing/modified_run_entire_clustering_algorithm_for_img_analysis.m
@@ -6,14 +6,14 @@ function [meets_acc_ratio,blind_pass_table] = modified_run_entire_clustering_alg
 %it avoids this by finding only 1 dictionary (with lowest boundry
 %threshold) and mask the spikes in all subsequent dictionary creation
 %thus skipping a lot of precessing work
+
 meets_acc_ratio = false;
 
 scale_factor = config.SCALE_FACTOR;
 dir_with_channel_recordings = config.DIR_WITH_OG_CHANNEL_RECORDINGS;
 num_dps = config.NUM_DPTS_TO_SLICE;
 
-%throttle computation for frontera memory issuies
-parpool('Processes', 40);
+
 
 %step 3: get list of existing files in the precomputed dir
 %doing this enables us to tell which parts of the algorithm can be
