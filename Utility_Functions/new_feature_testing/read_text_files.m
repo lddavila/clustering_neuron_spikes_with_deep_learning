4e7e4ddf4de5ab218d95575f6869eece30635a5f
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-09-14 14:29:51 -0600

    added reader for finished text files

---
+++ Utility_Functions/new_feature_testing/read_text_files.m
@@ -0,0 +1,17 @@
+function [] = read_text_files()
+fp_to_dir = "/scratch1/10595/lddavila/clustering_neuron_spikes_with_deep_learning/Default_Results_Dir/img_threshold_finding_incremental/accuracy_text_files/*.txt";
+all_txt_files = struct2table(dir(fp_to_dir));
+sliced_txt_files = slice_table_for_parallel_processing(all_txt_files,[]);
+final_table = [];
+parfor i=1:length(sliced_txt_files)
+    current_data = sliced_txt_files{i};
+    current_data_string = split(current_data{1,"name"},"_",2);
+    tetrode = "t"+current_data_string(2);
+    accuracy = str2double(current_data_string(4));
+    z_score = str2double(current_data_string(6));
+    channels = [str2double(current_data_string(8)),str2double(current_data_string(9)),str2double(current_data_string(10)),str2double(strrep(current_data_string(11),".txt",""))];
+    current_row = table(z_score,tetrode,accuracy,channels,'VariableNames',["Z Score","Tetrode","accuracy","channels"]);
+    final_table = [final_table;current_row];
+end
+par_save(fullfile("/scratch1/10595/lddavila/clustering_neuron_spikes_with_deep_learning/Default_Results_Dir/img_threshold_finding_incremental","table_of_image_accuracy_data.mat"),final_table);
+end
