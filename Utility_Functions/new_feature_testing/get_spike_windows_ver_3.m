e3aee16b6196fdc4841bedbc8b674a04516b54a4
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-09-16 13:44:24 -0600

    added status bar to new spike windows and also repressed warning by making the config file a parallel constant

--- Utility_Functions/new_feature_testing/get_spike_windows_ver_3.m
+++ Utility_Functions/new_feature_testing/get_spike_windows_ver_3.m
@@ -1,4 +1,4 @@
-function [] = get_spike_windows_ver_3(channels,desired_z_score,lowest_bound_spike_windows_dir,new_spike_windows_dir)
+function [] = get_spike_windows_ver_3(channels,desired_z_score,lowest_bound_spike_windows_dir,new_spike_windows_dir,config)
 
 %differs from get_spike_windows_ver_2 because instead of creating the spike windows array per channel
 %this function reads an existing spike windows z score and eliminates any
@@ -7,7 +7,13 @@ q = parallel.pool.DataQueue;
 afterEach(q,@print_status_bar)
 num_iterations = length(channels);
 print_status_bar(num_iterations,"get_spike_windows_ver_3.m")
+
+const_config = parallel.pool.Constant(config);
 parfor i=1:length(channels)
+    if isfile(fullfile(const_config.BLIND_PASS_DIR_PRECOMPUTED, "c"+string(channels(i))+".mat"))
+        send(q,[]);
+        continue;
+    end
     current_channel = channels(i);
     previously_found_spike_windows = importdata(fullfile(lowest_bound_spike_windows_dir,"c"+string(current_channel)+".mat"));
     spike_windows = previously_found_spike_windows(previously_found_spike_windows(:,5)>=desired_z_score,:);
