c9e153ebce7ae0bdee0aac5b7bb4e69399ba2e46
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-09-16 14:11:07 -0600

    added status bar feature

--- Utility_Functions/postprocessing/add_is_neuron_col.m
+++ Utility_Functions/postprocessing/add_is_neuron_col.m
@@ -1,11 +1,17 @@
 function [blind_pass_table] = add_is_neuron_col(blind_pass_table,config)
 array_of_is_neuron = nan(size(blind_pass_table,1),1);
 sliced_blind_pass_table = slice_table_for_parallel_processing(blind_pass_table,[]);
+config =parallel.pool.Constant(config);
+q = parallel.pool.DataQueue;
+afterEach(q,@print_message_using_dataqueue)
+num_iterations = size(blind_pass_table,1);
+print_status_bar(num_iterations,"add_is_neuron_col.m")
 parfor i=1:size(blind_pass_table,1)
     current_data = sliced_blind_pass_table{i};
     current_grades = current_data{1,"grades"}{1};
     [~,idx_of_MUA] = find(config.NAMES_OF_CURR_GRADES=="MUA or Not");
     array_of_is_neuron(i) = current_grades{idx_of_MUA};
+    send(q,[]);
 end
 blind_pass_table.("is_neuron") =array_of_is_neuron;
 end
