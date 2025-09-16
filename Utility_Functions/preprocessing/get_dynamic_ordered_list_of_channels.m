1d65c607cc4b9fa9dca1e490141dd82009a535b7
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-09-16 15:23:59 -0600

    added wildcard for exclusive .mat files

--- Utility_Functions/preprocessing/get_dynamic_ordered_list_of_channels.m
+++ Utility_Functions/preprocessing/get_dynamic_ordered_list_of_channels.m
@@ -1,6 +1,6 @@
 function [ordered_list_of_channels] = get_dynamic_ordered_list_of_channels(config)
 %get list of all available channels
-list_of_files_in_channel_dir = struct2table(dir(config.DIR_WITH_OG_CHANNEL_RECORDINGS));
+list_of_files_in_channel_dir = struct2table(dir(fullfile(config.DIR_WITH_OG_CHANNEL_RECORDINGS,"*.mat")));
 list_of_files_in_channel_dir(list_of_files_in_channel_dir{:,"name"}=="." | list_of_files_in_channel_dir{:,"name"}==".." ,:) = [];
 file_names_as_strings_split = strrep(split(string(list_of_files_in_channel_dir{:,"name"}),"."),"c","");
 
