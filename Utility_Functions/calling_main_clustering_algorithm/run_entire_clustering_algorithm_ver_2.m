50c8c55b71178b144bd70e761fbe26ca5dce96f8
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-09-16 15:20:27 -0600

    added check to avoid repeating spike windows for lowest bound

--- Utility_Functions/calling_main_clustering_algorithm/run_entire_clustering_algorithm_ver_2.m
+++ Utility_Functions/calling_main_clustering_algorithm/run_entire_clustering_algorithm_ver_2.m
@@ -84,18 +84,19 @@ get_lowest_bound_spike_windows(ordered_list_of_channels,lowest_bound_spikes_per_
 channels_without_formatting = str2double(strrep(strrep(ordered_list_of_channels,"c",""),".mat",""));
 if ~ismember(fullfile(precomputed_dir,"blind_pass.txt"),what_is_computed)
     for min_z_score=z_scores_to_check
-        if ~min_z_score==lower_bound_z_score
-        end
         % if what_is_pre_computed is not empty then we can skip several of the steps and just load the data
         %   each element of "what_is_precomputed" is a string telling you
-        %   what is already done 
+        %   what is already done
         % step 9c; Get all the data points from the potential spikes
-        beginning_time = tic;
-        spike_windows_dir = create_a_file_if_it_doesnt_exist_and_ret_abs_path(fullfile(precomputed_dir,"spike_windows min_z_score " + string(min_z_score) + " num dps "+ string(num_dps)));
-        get_spike_windows_ver_3(channels_without_formatting,min_z_score,lowest_bound_spike_windows_dir,spike_windows_dir,config);
-        end_time = toc(beginning_time);
-        fprintf("Finished getting spike windows for z score %.2f, it took %.2f seconds\n",min_z_score,end_time);
-
+        if ~min_z_score==lower_bound_z_score
+            beginning_time = tic;
+            spike_windows_dir = create_a_file_if_it_doesnt_exist_and_ret_abs_path(fullfile(precomputed_dir,"spike_windows min_z_score " + string(min_z_score) + " num dps "+ string(num_dps)));
+            get_spike_windows_ver_3(channels_without_formatting,min_z_score,lowest_bound_spike_windows_dir,spike_windows_dir,config);
+            end_time = toc(beginning_time);
+            fprintf("Finished getting spike windows for z score %.2f, it took %.2f seconds\n",min_z_score,end_time);
+        else
+            spike_windows_dir = lowest_bound_spike_windows_dir;
+        end
 
         % step 9d: get maps of each tetrode to its spikes
         beginning_time = tic;
