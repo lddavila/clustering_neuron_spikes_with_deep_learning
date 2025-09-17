9001d0e59b4ca8ddbe11ded0111ef17cb772bfb8
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-09-16 20:10:13 -0600

    added image development for merging nn

--- Utility_Functions/new_feature_testing/train_nn_to_identify_same_neuron_via_image_of_spikes.m
+++ Utility_Functions/new_feature_testing/train_nn_to_identify_same_neuron_via_image_of_spikes.m
@@ -21,7 +21,7 @@ all_comparisons = nchoosek(1:size(blind_pass_table,1),2);
 is_same_neuron = blind_pass_table{all_comparisons(:,1),"Max Overlap Unit"} == blind_pass_table{all_comparisons(:,2),"Max Overlap Unit"};
 
 %now set how many training samples you want
-num_training_samples = 100000;
+num_training_samples = 10000;
 
 %now from each class randomly sample num_training_samples/2
 rng(0);
@@ -29,10 +29,29 @@ number_of_samples_per_class = round(num_training_samples);
 is_same_neuron_indexes = find(is_same_neuron);
 is_not_same_neuron_samples = find(~is_same_neuron);
 
+
 random_groupable = all_comparisons(is_same_neuron_indexes(randperm(length(is_same_neuron_indexes),number_of_samples_per_class)),:);
 random_non_groupable = all_comparisons(is_not_same_neuron_samples(randperm(length(is_not_same_neuron_samples),number_of_samples_per_class)),:);
-all_training_values = [random_non_groupable;random_groupable];
+all_training_values = [[random_non_groupable,zeros(size(random_non_groupable,1),1)];[random_groupable,zeros(size(random_groupable,1),1)+1]];
 %shuffle the values
-all_training_values = all_training_values(randperm(size(all_training_values,1),size(all_training_values,1)));
+all_training_values = all_training_values(randperm(size(all_training_values,1),size(all_training_values,1)),:);
+
+%now get all mean waveforms available for the left and right cluster
+blind_pass_table_vars = string(blind_pass_table.Properties.VariableNames);
+cols_of_mean_wfs = contains(blind_pass_table_vars,"mean_waveform");
+mean_wf_cols = blind_pass_table_vars(cols_of_mean_wfs);
+left_mean_wf_cell_array = cell(length(mean_wf_cols),1);
+right_mean_wf_cell_array = cell(length(mean_wf_cols),1);
+for i=1:length(mean_wf_cols)
+    left_mean_wf_cell_array{i}= vertcat(blind_pass_table{all_training_values(:,1),mean_wf_cols(i)}{:});
+    right_mean_wf_cell_array{i}=  vertcat(blind_pass_table{all_training_values(:,2),mean_wf_cols(i)}{:});
+end
+
+%now we can get the training images for the 2 wfs
+dir_to_save_results_to = create_a_file_if_it_doesnt_exist_and_ret_abs_path(fullfile(config.parent_save_dir,"group_or_dont_based_on_wf"));
+dir_to_save_images_to = create_a_file_if_it_doesnt_exist_and_ret_abs_path(fullfile(dir_to_save_results_to,"training_images"));
+get_simple_wf_plots(left_mean_wf_cell_array,right_mean_wf_cell_array,dir_to_save_images_to);
+
+
 
 end
