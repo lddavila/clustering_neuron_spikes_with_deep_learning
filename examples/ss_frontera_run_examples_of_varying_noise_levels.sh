19b9f51252b134f09c15a4bef15677dfd76ad147
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-09-24 15:00:39 -0600

    fixed ss

--- examples/ss_frontera_run_examples_of_varying_noise_levels.sh
+++ examples/ss_frontera_run_examples_of_varying_noise_levels.sh
@@ -6,4 +6,4 @@
 #SBATCH -e output.txt 
 #SBATCH -t 48:00:00
 module load matlab/2023b
-matlab -batch "run_examples_of_varying_noise_levels();exit;"
+matlab -batch "run_examples_of_varying_noise_level();exit;"
