2baaadb2e94aa013a217e8fb68b2bd37f3810790
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-09-24 20:41:10 -0600

    added scripts to run other ss

---
+++ test_other_spike_sorters/run_all_other_ss_on_available_recordings.py
@@ -0,0 +1,58 @@
+from spikeinterface.extractors import toy_example
+from run_ks4_using_docker_container import run_various_spike_sorters
+import os
+from pathlib import Path
+import spikeinterface as si
+import spikeinterface.extractors as se
+
+#create a simple recording and ground truth sorting
+#toy_example_rec, toy_example_sorting_true = toy_example(duration=30, num_channels=64, num_segments=1, seed=0)
+
+
+  #  sorting = run_sorter_jobs(
+  #      sorter_name="kilosort4",
+  #      recording=rec,
+  #      docker_image=True,                 # or: "spikeinterface/kilosort4-base:4.0.38_cuda-12.0.0"
+  #      installation_mode="pypi",           # ← key change
+  #      remove_existing_folder=True,       # ← correct flag
+  #      verbose=True,
+  #      torch_device="cpu",                # ← run KS4 on CPU (slow)
+
+#get the base directory of this project
+#define the base directory of this project
+current_script_fp = Path(__file__).absolute()
+split_current_fp = current_script_fp.parts
+base_index = split_current_fp.index('clustering_neuron_spikes_with_deep_learning')
+base_dir = Path(*split_current_fp[:base_index + 1])
+
+#define the directory with the h5 files
+data_dir = base_dir / "Data"
+
+# get a list of all the h5 files in the data directory
+h5_files = list(data_dir.glob("*.h5"))
+
+#remove the template file if it exists
+h5_files = [f for f in h5_files if 'templates_300_neuropixels.h5' not in f.name.lower()]
+
+#print the h5 files found
+print(f"Found {len(h5_files)} h5 files: {[f.name for f in h5_files]}")
+
+
+#define a list of sorters to run
+list_of_sorters = ['kilosort4', 'mountainsort5', 'ironclust', 'herdingspikes', 'spyking-circus']
+
+
+#now import each h5 file as a recording and run the sorters on it
+for i, rec_fp in enumerate(h5_files):
+    #get the name of the file without the full path
+    head, tail = os.path.split(rec_fp)
+    tail = tail.replace('.h5', '')
+    out_dir  = base_dir / "Default_Results_Dir" / (tail + "_results")
+    rec = se.MEArecRecordingExtractor('mearec_sim.h5')
+    sorting_true = se.MEArecSortingExtractor('mearec_sim.h5')
+    #add these to the job list
+    job_list = []
+    for i in list_of_sorters:
+        job_list.append({'sorter_name': i, 'recording': rec, 'docker_image':True, 'installation_mode':"pypi", 'torch_device':"cpu", 'remove_existing_folder':True, 'verbose':True})
+
+    run_various_spike_sorters(rec, sorting_true, job_list, base_dir,out_dir=out_dir)
---
+++ test_other_spike_sorters/run_ks4_using_docker_container.py
@@ -0,0 +1,37 @@
+
+import shutil
+from spikeinterface.extractors import toy_example
+from spikeinterface import create_sorting_analyzer
+#from spikeinterface import compare_sorter_to_ground_truth
+from spikeinterface.sorters import run_sorter_jobs
+import spikeinterface.sorters.runsorter as _rs
+import spikeinterface as sc
+import os
+from spikeinterface.comparison import compare_sorter_to_ground_truth
+import numpy as np
+
+
+def run_various_spike_sorters(rec,sorting_true,job_list_current,base_dir,out_dir,ground_truth):
+    _rs.has_nvidia = lambda: False  # No GPU on this box → make SI skip CUDA paths cleanly
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    # start fresh instead of using "overwrite" on run_sorter
+    shutil.rmtree(out_dir, ignore_errors=True)
+    out_dir.mkdir(parents=True, exist_ok=True)
+
+    #for every dictionary in job_list add the folder key
+    for i, job in enumerate(job_list_current):
+        job['folder'] = str(out_dir / f"{job['sorter_name'+'_output']}")
+    
+    sortings = run_sorter_jobs(job_list=job_list_current, verbose=True)
+
+    #now for every sorting in sortings do a ground truth comparison
+    for sorter_name, sorting in sortings.items():
+        print(f"Results for {sorter_name}")
+        cmp_to_gt_results = compare_sorter_to_ground_truth(sorting_true, sorting, exhaustive_gt=True)
+        sorting = sorting.save(folder=str(out_dir / f"{sorter_name}_output" / f"{sorter_name}_sorting"), overwrite=True)
+        perf = cmp_to_gt_results.get_performance()
+        save_path = out_dir / f"{sorter_name}_performance.npy"
+        np.save(save_path, perf)
+        print(f"Performance saved to {save_path}")
+        print(perf)
