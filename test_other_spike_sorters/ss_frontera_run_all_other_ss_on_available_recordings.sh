948ca0bfc5b843eec0aa65b093967448a81c6552
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-09-25 14:01:46 -0600

    added new ss

--- test_other_spike_sorters/ss_frontera_run_all_other_ss_on_available_recordings.sh
+++ test_other_spike_sorters/ss_frontera_run_all_other_ss_on_available_recordings.sh
@@ -7,7 +7,7 @@
 #SBATCH -t 1:00:00
 export TMPDIR=/scratch/$USER/temp
 mkdir -p $TMPDIR
-source /gpfs/scratch/afriedman/spike_gen_work/my_env/bin/activate
+source /spike_gen_work/my_env/bin/activate
 echo "Python path: $(which python)"
 python -m pip list | grep MEArec
 python run_all_other_ss_on_available_recordings.py
