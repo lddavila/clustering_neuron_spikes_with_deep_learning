7f5905af6cbad3744c27c22ae132aa6da25d3c7d
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-09-26 10:47:54 -0600

    reduced runtime

--- examples/ss_frontera_run_examples_of_varying_noise_levels.sh
+++ examples/ss_frontera_run_examples_of_varying_noise_levels.sh
@@ -4,6 +4,6 @@
 #SBATCH -J reading
 #SBATCH -o output.txt 
 #SBATCH -e output.txt 
-#SBATCH -t 24:00:00
+#SBATCH -t 1:00:00
 module load matlab/2023b
 matlab -batch "run_examples_of_varying_noise_level();exit;"
