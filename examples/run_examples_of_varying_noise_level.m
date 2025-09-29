669be0a9e4f1004dec8b62304065ed0bbc823c44
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-09-29 15:51:31 -0600

    fixed improper fpth setting

--- examples/run_examples_of_varying_noise_level.m
+++ examples/run_examples_of_varying_noise_level.m
@@ -21,13 +21,10 @@ for i=1:5:8
     config.BLIND_PASS_DIR_PRECOMPUTED = fullfile(config.BLIND_PASS_DIR_PRECOMPUTED,config.RECORDING_NAME);
     startup;
     disp("Finished Setting Recording Name")
-    %replace the base file path
-    config.base_file_path = strrep(config.base_file_path,"cnheaton","afriedman");
-    config.base_file_path = strrep(config.base_file_path,"lddavila","afriedman");
     % (OPTIONAL STEP 2 CONTINUED) SET THE filepath of the ground truth files if your recording is simulated and they are available
-    config.GT_FP = fullfile(config.base_file_path,"Data",config.RECORDING_NAME,"ground_truth","ground_truth.mat");
-    config.TIMESTAMP_FP = fullfile(config.base_file_path,"Data",config.RECORDING_NAME,"timestamps","timestamps.mat");
-    config.DIR_WITH_OG_CHANNEL_RECORDINGS = fullfile(config.base_file_path,"Data",config.RECORDING_NAME,"recordings_by_channel");
+    config.GT_FP = fullfile(strrep(strrep(config.base_file_path,"cnheaton","afriedman"),"lddavila","afriedman"),"Data",config.RECORDING_NAME,"ground_truth","ground_truth.mat");
+    config.TIMESTAMP_FP = fullfile(strrep(strrep(config.base_file_path,"cnheaton","afriedman"),"lddavila","afriedman"),"Data",config.RECORDING_NAME,"timestamps","timestamps.mat");
+    config.DIR_WITH_OG_CHANNEL_RECORDINGS = fullfile(strrep(strrep(config.base_file_path,"cnheaton","afriedman"),"lddavila","afriedman"),"Data",config.RECORDING_NAME,"recordings_by_channel");
     disp("Finished Setting directories")
    
     % Step 3: Download Necessary Data
