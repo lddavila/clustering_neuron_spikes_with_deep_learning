9533423ad07f30fa283a91e182a27bb28c057b34
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-09-29 16:15:56 -0600

    fixed typo

--- examples/run_examples_of_varying_noise_level.m
+++ examples/run_examples_of_varying_noise_level.m
@@ -33,7 +33,7 @@ for i=beginning:the_end
     % (OPTIONAL STEP 2 CONTINUED) SET THE filepath of the ground truth files if your recording is simulated and they are available
     config.GT_FP = fullfile(strrep(strrep(config.base_file_path,"cnheaton","afriedman"),"lddavila","afriedman"),"Data",config.RECORDING_NAME,"ground_truth","ground_truth.mat");
     config.TIMESTAMP_FP = fullfile(strrep(strrep(config.base_file_path,"cnheaton","afriedman"),"lddavila","afriedman"),"Data",config.RECORDING_NAME,"timestamps","timestamps.mat");
-    dips("TS fp");
+    disp("TS fp");
     disp(config.TIMESTAMP_FP);
     config.DIR_WITH_OG_CHANNEL_RECORDINGS = fullfile(strrep(strrep(config.base_file_path,"cnheaton","afriedman"),"lddavila","afriedman"),"Data",config.RECORDING_NAME,"recordings_by_channel");
     disp("Finished Setting directories")
