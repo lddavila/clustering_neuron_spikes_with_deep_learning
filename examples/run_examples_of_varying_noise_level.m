6e6e35b381e9d1438df7ae30661c10c54bb1dc67
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-09-30 18:03:15 -0600

    fixed improper name reference

--- examples/run_examples_of_varying_noise_level.m
+++ examples/run_examples_of_varying_noise_level.m
@@ -44,7 +44,7 @@ for i=beginning:the_end
     [blind_pass_table,fp_to_bp_table,config] = run_entire_clustering_algorithm_ver_2(config);
 
     %add the recording name to the blind pass table
-    blind_pass_table.recording_name = repelem(config.recording_name,size(blind_pass_table,1),1);
+    blind_pass_table.recording_name = repelem(config.RECORDING_NAME,size(blind_pass_table,1),1);
     end_time = toc(very_beginning_time);
     fprintf("Finished running blind pass it took %f seconds\n",end_time)
     % (OPTIONAL STEP 5 CONTINUED) Get max overlap unit and accuracy cols for the neurons
