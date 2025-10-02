0c5f55792d65d4e18adc0976c700a78b5c2b8da8
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-10-01 21:36:25 -0600

    reintroduced parfor

--- Utility_Functions/postprocessing/add_is_neuron_col.m
+++ Utility_Functions/postprocessing/add_is_neuron_col.m
@@ -6,7 +6,7 @@ q = parallel.pool.DataQueue;
 afterEach(q,@print_status_bar)
 num_iterations = size(blind_pass_table,1);
 print_status_bar(num_iterations,"add_is_neuron_col.m")
-for i=1:size(blind_pass_table,1)
+parfor i=1:size(blind_pass_table,1)
     current_data = sliced_blind_pass_table{i};
     current_grades = current_data{1,"grades"}{1};
     [~,idx_of_MUA] = find(config.Value.NAMES_OF_CURR_GRADES=="MUA or Not");
