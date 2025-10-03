66b7507b0729a928820b2659dd512929a3494e29
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-10-03 13:49:59 -0600

    added osme debugging text

--- Grading_scripts/get_grades_for_nth_pass_of_clustering_ver_2.m
+++ Grading_scripts/get_grades_for_nth_pass_of_clustering_ver_2.m
@@ -15,6 +15,7 @@ q = parallel.pool.DataQueue;
 afterEach(q,@print_status_bar)
 print_status_bar(num_iterations,"get_grades_for_nth_pass_of_clustering_ver_2.m")
 parfor i=1:size(sliced_blind_pass_table,1)
+    disp("Starting grading")
     current_data = sliced_blind_pass_table{i};
     current_tetrode = current_data{1,"Tetrode"};
 
