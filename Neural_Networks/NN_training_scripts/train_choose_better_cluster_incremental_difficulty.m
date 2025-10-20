859782f22780cf6ebdd064de1db887ee725445e5
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-10-19 19:59:33 -0600

    specified threads

--- Neural_Networks/NN_training_scripts/train_choose_better_cluster_incremental_difficulty.m
+++ Neural_Networks/NN_training_scripts/train_choose_better_cluster_incremental_difficulty.m
@@ -178,6 +178,7 @@ for difficulty_level=length(cell_array_of_accuracy_magnitudes):-1:1
 
     %now with all this assembled we can actually begin training
     disp("Beginning training on difficulty_level:"+string(difficulty_level))
+    parpool('Threads',num_workers);
      parfor i=1:size(permutations_table,1)
          if ~array_of_continue_training(i)
              continue;
