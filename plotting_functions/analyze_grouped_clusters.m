357fdb43e1cc10248f20095be567f8a5bb9ca5e6
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-11-07 13:11:13 -0700

    modified logical grouping tests to work on incremental accuracy

--- plotting_functions/analyze_grouped_clusters.m
+++ plotting_functions/analyze_grouped_clusters.m
@@ -1,25 +1,27 @@
 function [statistics_per_group,is_in_table] = analyze_grouped_clusters(cell_array_of_grouped_clusters,ground_truth_cell_array)
 
-group_names = string(1:size(cell_array_of_grouped_clusters,2)).';
-group_unit_composition = cell(size(cell_array_of_grouped_clusters,2),1);
-is_made_up_of_single = zeros(size(cell_array_of_grouped_clusters,2),1);
-majority_percentage = zeros(size(cell_array_of_grouped_clusters,2),1);
-group_size = zeros(size(cell_array_of_grouped_clusters,2),1);
-dominant_unit = zeros(size(cell_array_of_grouped_clusters,2),1);
-for i=1:size(cell_array_of_grouped_clusters,2)
+group_names = string(1:length(cell_array_of_grouped_clusters)).';
+group_unit_composition = cell(length(cell_array_of_grouped_clusters),1);
+is_made_up_of_single = zeros(length(cell_array_of_grouped_clusters),1);
+majority_percentage = zeros(length(cell_array_of_grouped_clusters),1);
+group_size = zeros(length(cell_array_of_grouped_clusters),1);
+dominant_unit = zeros(length(cell_array_of_grouped_clusters),1);
+for i=1:length(cell_array_of_grouped_clusters)
     current_group = cell_array_of_grouped_clusters{i};
-    unit_counts_in_group = groupcounts(current_group,"Max Overlap Unit");
+    unit_counts_in_group = groupcounts(current_group,"Max_Overlap_Unit");
     group_unit_composition{i} = unit_counts_in_group;
     if size(unit_counts_in_group,1) == 1
         is_made_up_of_single(i) = 1;
     end
     [majority_percentage(i),max_index] = max(unit_counts_in_group{:,"Percent"});
     group_size(i) = size(current_group,1);
-    dominant_unit(i) = unit_counts_in_group{max_index,"Max Overlap Unit"};
+    dominant_unit(i) = unit_counts_in_group{max_index,"Max_Overlap_Unit"};
 end
 statistics_per_group = table(group_names,dominant_unit,majority_percentage,group_size,group_unit_composition,is_made_up_of_single,...
     'VariableNames', ...
     ["Group Number","Dominant Unit","Max Percentage","# Members","Unit Breakdown","All Same Unit"]);
+
+statistics_per_group = sortrows(statistics_per_group,"Dominant Unit");
 
 ground_truth_units_list = 1:size(ground_truth_cell_array,2);
 is_in_matrix = nan(1,size(ground_truth_units_list,2));
--- plotting_functions/view_group_breakdown.m
+++ plotting_functions/view_group_breakdown.m
@@ -2,16 +2,16 @@ function [] = view_group_breakdown(statistics_table,selection,bar_plot_axes,mean
 
 unit_breakdown = statistics_table{selection(1),"Unit Breakdown"}{1};
 bar_plot_values = unit_breakdown{:,"GroupCount"};
-bar_plot_labels = unit_breakdown{:,"Max Overlap Unit"};
+bar_plot_labels = unit_breakdown{:,"Max_Overlap_Unit"};
 bar(bar_plot_axes,bar_plot_labels,bar_plot_values./sum(bar_plot_values,"all"));
 
-unique_units_in_group = unique(group_data{:,"Max Overlap Unit"});
+unique_units_in_group = unique(group_data{:,"Max_Overlap_Unit"});
 legend_string = strcat("Unit",string(unique_units_in_group));
 
 for j=1:length(mean_waveform_plot)
     hold(mean_waveform_plot(j),'off');
     for i=1:size(unique_units_in_group,1)
-        mean_waveform = cell2mat(group_data{group_data{:,"Max Overlap Unit"}==unique_units_in_group(i),"mean_waveform_rep_wire_"+string(j)});
+        mean_waveform = cell2mat(group_data{group_data{:,"Max_Overlap_Unit"}==unique_units_in_group(i),"mean_waveform_rep_wire_"+string(j)});
         plot(mean_waveform_plot(j),mean(mean_waveform,1));
         hold(mean_waveform_plot(j),'on');
     end
