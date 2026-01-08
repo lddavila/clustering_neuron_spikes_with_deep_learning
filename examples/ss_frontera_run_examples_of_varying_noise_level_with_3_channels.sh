67ebb79da495259862d0c4e8e5513b6701b8dd25
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2026-01-08 16:10:42 -0700

    fixed matlab call

--- examples/ss_frontera_run_examples_of_varying_noise_level_with_3_channels.sh
+++ examples/ss_frontera_run_examples_of_varying_noise_level_with_3_channels.sh
@@ -5,5 +5,5 @@
 #SBATCH -o output.txt 
 #SBATCH -e output.txt 
 #SBATCH -t 48:00:00
-module load matlab/R2024b
+module load matlab/R2023b
 matlab -batch "run_examples_of_varying_noise_level_with_3_channels();exit;"
