60bbcfb866176c9e9bbab26c208d1fe18d85e40f
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2026-01-08 16:03:44 -0700

    added frontera

---
+++ examples/ss_frontera_run_examples_of_varying_noise_level_with_3_channels.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+#SBATCH -N 1
+#SBATCH -p small
+#SBATCH -J examples
+#SBATCH -o output.txt 
+#SBATCH -e output.txt 
+#SBATCH -t 48:00:00
+module load matlab/R2024b
+matlab -batch "run_examples_of_varying_noise_level_with_3_channels();exit;"
