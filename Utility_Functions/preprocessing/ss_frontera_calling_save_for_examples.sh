e2cf1a465badaa765a4ad125dc38f8ecb81ee330
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2026-01-09 14:13:23 -0700

    adjusted ss

--- Utility_Functions/preprocessing/ss_frontera_calling_save_for_examples.sh
+++ Utility_Functions/preprocessing/ss_frontera_calling_save_for_examples.sh
@@ -6,7 +6,7 @@
 #SBATCH -t 48:00:000
 export TMPDIR=/scratch2/10595/$USER/clustering_neuron_spikes_with_deep_learning/Data
 mkdir -p $TMPDIR
-source /scratch2/10595/$USER/clustering_neuron_spikes_with_deep_learnin/Data/spike_env/bin/activate
+source /scratch2/10595/$USER/clustering_neuron_spikes_with_deep_learning/Data/spike_env/bin/activate
 echo "Python path: $(which python)"
 python -m pip list | grep MEArec
 python calling_save_for_examples.py
