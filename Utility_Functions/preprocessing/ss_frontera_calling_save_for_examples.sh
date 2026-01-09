f3171733afd5dd455166b296e8578c71f4bede88
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2026-01-09 14:04:54 -0700

    fixed ss path

--- Utility_Functions/preprocessing/ss_frontera_calling_save_for_examples.sh
+++ Utility_Functions/preprocessing/ss_frontera_calling_save_for_examples.sh
@@ -4,9 +4,9 @@
 #SBATCH -o output.txt 
 #SBATCH -e output.txt
 #SBATCH -t 48:00:000
-export TMPDIR=/scratch2/$USER/temp
+export TMPDIR=/scratch2/10595/$USER/clustering_neuron_spikes_with_deep_learning/Data
 mkdir -p $TMPDIR
-source /gpfs/scratch/afriedman/spike_gen_work/my_env/bin/activate
+source /scratch2/10595/$USER/clustering_neuron_spikes_with_deep_learnin/Data/spike_env/bin/activate
 echo "Python path: $(which python)"
 python -m pip list | grep MEArec
 python calling_save_for_examples.py
