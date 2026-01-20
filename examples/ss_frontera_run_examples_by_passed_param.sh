8bd566dae539b2c51b078e7122a30ae9d1cd477e
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2026-01-20 14:58:12 -0700

    added ss

---
+++ examples/ss_frontera_run_examples_by_passed_param.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+#SBATCH -N 1
+#SBATCH -p small
+#SBATCH -J examples
+#SBATCH -o output_1.txt 
+#SBATCH -e output_1.txt 
+#SBATCH -t 48:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=lddavila@miners.utep.edu
+module load matlab/2023b
+matlab -batch "run_examples_by_passed_param(1);exit;"
+
