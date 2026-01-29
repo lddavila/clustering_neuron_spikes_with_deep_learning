7f2a775af997860d16f6ef2a6ea9500fb5f7c3e1
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2026-01-28 19:09:45 -0700

    modified for new ss

--- Utility_Functions/new_feature_testing/ss_frontera_find_threshold_computationally.sh
+++ Utility_Functions/new_feature_testing/ss_frontera_find_threshold_computationally.sh
@@ -8,4 +8,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=lddavila@miners.utep.edu
 module load matlab/2023b
-matlab -batch "find_threshold_computationally('5_600Neuron300SecondRecordingWithLevel5Noise');exit;"
+matlab -batch "find_threshold_computationally('10_600Neuron300SecondRecordingWithLevel10Noise');exit;"
