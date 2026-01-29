e937477c8476ce53e388b394dc3b7dc9e9f2d3dc
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2026-01-28 21:31:16 -0700

    changed output file

--- Utility_Functions/new_feature_testing/ss_frontera_find_threshold_computationally.sh
+++ Utility_Functions/new_feature_testing/ss_frontera_find_threshold_computationally.sh
@@ -2,8 +2,8 @@
 #SBATCH -N 1
 #SBATCH -p small
 #SBATCH -J rec_10
-#SBATCH -o output.txt 
-#SBATCH -e output.txt 
+#SBATCH -o output_10.txt 
+#SBATCH -e output_10.txt 
 #SBATCH -t 48:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=lddavila@miners.utep.edu
