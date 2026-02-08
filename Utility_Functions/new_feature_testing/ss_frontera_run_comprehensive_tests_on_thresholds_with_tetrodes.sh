3ac5bd679bb5ea78025714f8bc2c94b5ba9cb91f
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2026-02-07 21:11:46 -0700

    updated for new rec

--- Utility_Functions/new_feature_testing/ss_frontera_run_comprehensive_tests_on_thresholds_with_tetrodes.sh
+++ Utility_Functions/new_feature_testing/ss_frontera_run_comprehensive_tests_on_thresholds_with_tetrodes.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 #SBATCH -N 1
 #SBATCH -p small
-#SBATCH -J rec_10
-#SBATCH -o output_10.txt 
-#SBATCH -e output_10.txt 
+#SBATCH -J rec_1
+#SBATCH -o output_1.txt 
+#SBATCH -e output_1.txt 
 #SBATCH -t 48:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=lddavila@miners.utep.edu
