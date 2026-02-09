2368d0492d0f9e928d729ec2cc6e89eae1bff885
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2026-02-09 15:46:29 -0700

    added for longer

--- Utility_Functions/new_feature_testing/run_comprehensive_tests_on_thresholds_with_tetrodes.m
+++ Utility_Functions/new_feature_testing/run_comprehensive_tests_on_thresholds_with_tetrodes.m
@@ -28,13 +28,13 @@ list_of_all_files = list_of_all_files(string(list_of_all_files{:,"name"})=="neur
 
 %meta deta parameters for testing
 num_neurons_to_test_per_rec = 5;
-number_of_channels_to_try = 2:1:6;
+number_of_channels_to_try = [4,3,2,5,6];
 multipliers_to_try = 6:.1:15;
 config.Multipliers = multipliers_to_try;
 all_table = {};
 recording_names_array = ["10_600Neuron300SecondRecordingWithLevel10Noise","1_600Neuron300SecondRecordingWithLevel1Noise","5_600Neuron300SecondRecordingWithLevel5Noise"];
 
-for rec_count=3:height(list_of_all_files)
+for rec_count=1:height(list_of_all_files)
     %set the randoms seed for reproducability
     rng(0);
     unit_loc = importdata(fullfile(list_of_all_files{rec_count,"folder"}{1},list_of_all_files{rec_count,"name"}{1}));
