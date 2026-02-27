fd91069fed548695932289a930096abf4d15de1d
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2026-02-26 20:33:02 -0700

    modified par_save to check size before saving appropriately

--- Utility_Functions/postprocessing/par_save.m
+++ Utility_Functions/postprocessing/par_save.m
@@ -4,7 +4,21 @@ if ~strcmp(file_save_name_as_char(end-3:end), '.mat') %if it's missing the .mat 
     file_save_name = file_save_name+".mat";
 end
 %check if the file is too large to save in normal format
-save(file_save_name,"data_to_save", '-v7.3', '-nocompression');
+% Get information about the variable using whos
+varInfo = whos("data_to_save");
 
+% The size is in the 'bytes' field of the structure
+varSizeInBytes = varInfo.bytes;
+
+% Define the 2 GB limit in bytes (2 * 1024 * 1024 * 1024)
+limitBytes = 1.5 * 1024^3; 
+
+% Check if the variable size is less than the limit
+isLessThan2GB = varSizeInBytes < limitBytes;
+if isLessThan2GB
+    save(file_save_name,"data_to_save");
+else
+    save(file_save_name,"data_to_save", '-v7.3', '-nocompression');
+end
 
 end
