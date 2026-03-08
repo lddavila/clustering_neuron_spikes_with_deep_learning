713de5abb5330cde07a426071cc7a9d5b205d86d
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2026-03-08 14:03:23 -0600

    new ss

--- examples/ss_frontera_run_recordings_based_on_input.sh
+++ examples/ss_frontera_run_recordings_based_on_input.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
 #SBATCH -N 1
 #SBATCH -p small
-#SBATCH -J rec_8_ic
-#SBATCH -o output_8.txt 
-#SBATCH -e output_8.txt
+#SBATCH -J rec_7_ic
+#SBATCH -o output_7.txt 
+#SBATCH -e output_7.txt
 #SBATCH -t 48:00:00
 #SBATCH --mail-type=ALL	
 #SBATCH --mail-user=lddavila@miners.utep.edu
 module load matlab/2023b
-matlab -batch "run_recordings_based_on_input(8);exit;"
+matlab -batch "run_recordings_based_on_input(7);exit;"
