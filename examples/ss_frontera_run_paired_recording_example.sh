a3128e14f346d80d52426186efed27883deb1506
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2026-03-10 22:43:46 -0600

    adjusted ss

--- examples/ss_frontera_run_paired_recording_example.sh
+++ examples/ss_frontera_run_paired_recording_example.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 #SBATCH -N 1
 #SBATCH -p small
-#SBATCH -J paired_rec_1
-#SBATCH -o paired_output_1.txt 
-#SBATCH -e paired_output_1.txt
+#SBATCH -J paired_rec_10
+#SBATCH -o paired_output_10.txt 
+#SBATCH -e paired_output_10.txt
 #SBATCH -t 48:00:00
 #SBATCH --mail-type=ALL	
 #SBATCH --mail-user=lddavila@miners.utep.edu
