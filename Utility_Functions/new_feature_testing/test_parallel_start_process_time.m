11c7bd51ab3e9a1427f1dacafa44e4b477c83391
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2026-06-04 09:31:45 -0600

    added utility to check server start times

---
+++ Utility_Functions/new_feature_testing/test_parallel_start_process_time.m
@@ -0,0 +1,32 @@
+generated_path = genpath(root_dir);
+utility_dir = fullfile(root_dir, "Utility_Functions");
+
+% Broad path
+
+addpath(generated_path);
+
+poolobj = gcp("nocreate");
+if ~isempty(poolobj)
+    delete(poolobj);
+end
+
+time_start = tic;
+poolobj = parpool;
+broad_path_pool_startup = toc(time_start);
+
+delete(poolobj);
+rmpath(generated_path);
+
+% Specialized path
+
+addpath(utility_dir);
+
+time_start = tic;
+poolobj = parpool;
+specialized_path_pool_startup = toc(time_start);
+
+delete(poolobj);
+rmpath(utility_dir);
+
+fprintf("Broad path pool startup:       %.2f seconds\n", broad_path_pool_startup);
+fprintf("Specialized path pool startup: %.2f seconds\n", specialized_path_pool_startup);
