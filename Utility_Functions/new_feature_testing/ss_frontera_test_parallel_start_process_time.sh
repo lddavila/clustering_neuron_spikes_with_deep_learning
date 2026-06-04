3099c7fc368302dedc89b3bfb4d0f29c1771b934
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2026-06-04 09:41:14 -0600

    added ss

---
+++ Utility_Functions/new_feature_testing/ss_frontera_test_parallel_start_process_time.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+#SBATCH -N 1
+#SBATCH -p small
+#SBATCH -J st_tm_tst
+#SBATCH -o output.txt 
+#SBATCH -e output.txt 
+#SBATCH -t 48:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=lddavila@miners.utep.edu
+module load matlab/2023b
+matlab -batch "run_examples_of_varying_noise_level();exit;"
