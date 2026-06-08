6d4c75e817f3a2228459ff88ce4c04d904996ae4
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2026-06-07 23:41:48 -0600

    fixed ss

--- Utility_Functions/new_feature_testing/ss_ls6_test_ic_on_rec_10.sh
+++ Utility_Functions/new_feature_testing/ss_ls6_test_ic_on_rec_10.sh
@@ -7,5 +7,6 @@
 #SBATCH -t 48:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=lddavila@miners.utep.edu
+#SBATCH -A CCR26037
 module load matlab/2023a
 matlab -batch "test_ic_on_rec_10(10,true);exit;"
