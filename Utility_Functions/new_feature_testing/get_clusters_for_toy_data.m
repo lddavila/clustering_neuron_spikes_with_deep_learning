069edc4e4dc30dd8e2c0ffa78d08e054cb6745f6
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2026-06-08 17:28:35 -0600

    increased @ workers

--- Utility_Functions/new_feature_testing/get_clusters_for_toy_data.m
+++ Utility_Functions/new_feature_testing/get_clusters_for_toy_data.m
@@ -1,106 +1,46 @@
 function [the_clusters] = get_clusters_for_toy_data(the_subsets,peaks)
-for k = 1:length(the_subsets)
-    filtered_idx = the_subsets{k};
-
-    % For each cluster, remove its mean + num_std*std for each of the peak
-    % dimensions.
-    for c = 1:length(the_clusters)
-        cluster_idx = the_clusters{c};
-
-        cp = peaks(cluster_idx, :);
-        coeff = pca(cp);
-        the_peak_pcs = nan(size(peaks, 1), min([size(the_aligned_spikes,1),2]));
-        for p = 1:size(the_peak_pcs,2)
-            the_peak_pcs(:, p) = peaks * coeff(:, p);
+  % disp("finished core cluster loop")
+    level = 1; %start the level
+    done = false; %by default you are not done
+    start_cl = struct(); %create a starting cluster struct object
+    start_cl.subclust = true; %set the subcluster property of the starting cluster equal to true
+    start_cl.idx = 1:size(peaks, 1); % put all spikes in spike_aligned into the initial cluster 
+    clusters = {start_cl}; %the initial clusters to be subclustered is just the starting cluster object
+    while ~done && level <= the_new_config.MAX_SUBCLUSTER_DEPTH % by default this is 5
+        % disp("entered while core_cluster_loop.m ")
+        full_config.current_level = level;
+        if level == 1
+            cluster_ns = the_new_config.CLUSTER_NS; %default value is 6
+        else
+            cluster_ns = the_new_config.SUBCLUSTER_NS; %default subcluster number is 4
         end
-        data = [peaks, the_peak_pcs];
+        next_clusters = {}; %will store each pass of clusters found by core_cluster
+        done = true; %for stopping purposes 
+        for k = 1:length(clusters) %on first pass takes the entire data set as a cluster
+            cl = clusters{k}; %take cluster k from the data set
+            if cl.subclust %check if cl should be sub clustered again 
+                done = false; %make sure the algorithm doesn't stop because the current cluster needs to be subclustered again
+                [subclusters,full_config]= core_cluster(the_spike_aligned(:, cl.idx, :), cluster_ns, cl.idx, the_extract_features_fn, the_new_config,the_peak_pcs_file_name,full_config); %get back subclusters from initial clutster
+                next_clusters = [next_clusters subclusters]; %add new subclusters to the list of clusters
 
-        ints = true(total_num_spikes, 1);
+            else
+                next_clusters = [next_clusters cl]; %if cl doesn't/can't be subclustered then just add it back to the next_clusters list as it is "completed"
+            end
+
+        end
+        clusters = next_clusters; %overwrite the clusters you began with
+        %this is important because it ensures that you will continue to subcluster the clusters which are still subclusterable, and not keep reclustering the same clusters again and again
+        % config,aligned,cluster_idx_struct,which_subset
+        % disp("about to create_Cluster_plots_with_accuracy_while_clustering_and_sub_clu")
+        if full_config.has_ground_truth && full_config.debug_with_ground_truth
+            create_cluster_plots_with_accuracy_while_clustering_and_sub_clu(full_config,the_spike_aligned,next_clusters,string(level))
+            full_config.plot_counter = full_config.plot_counter+1;
+        end
+        % disp("Finished creating the plot")
+        level = level + 1; %increase the subcluster level
         % close all;
-        % plot_clusters_spike_refinement("Before Refinement",c,peaks,filtered_idx,4);
-        %plot_aligned_for_refinment("Before Refinment",c,aligned,filtered_idx,4)
-        for d = 1:size(data, 2) %cycle through the features
-            feature = data(:, d);%index a feature column
-            cluster_feature = feature(cluster_idx); %get the feature for the current cluster
-            d_min = min(cluster_feature); %get the lower bound of the feature for the cluster
-            d_max = max(cluster_feature); %get the uppre bound of the feature for the cluster
-
-            d_mean = mean(cluster_feature); %get the mean of the feature for the cluster
-            d_std = std(cluster_feature); %get the std of the mean of the feature for the cluster
-
-            d_min = max(d_min, d_mean - num_std*d_std); %get the smallest value between the min cluster feature and the mean cluster feature - 3 * cluster featre_std
-            d_max = min(d_max, d_mean + num_std*d_std); %get the max value between the max cluster feature and the mean cluster feature + 3 * cluster featre_std
-
-            ints = ints & d_min < feature & feature < d_max; %check which cluster member's features lie between d_min and d_max
-            %but the interesting part is that you check for this in between condition along every dimension of the cluster
-            % therefore to pass for a cluster spike to survive the filter it must always be in between these values regardless of dimension
-        end
-        remove_spikes = union(find(ints), cluster_idx);
-        filtered_idx = setdiff(filtered_idx, remove_spikes);
-        %plot_clusters_spike_refinement("After Refinement",c,peaks,filtered_idx,4);
-        %plot_aligned_for_refinment("After Refinment",c,aligned,filtered_idx,4)
     end
-
-    % Run clustering with this pass of filtering as specified in
-    % subsets, with everything removed above.
-    full_config.which_subset = k;
-    [cf, bad_tmp,full_config,is_bad_cluster] = run_clustering(the_aligned_spikes, filtered_idx, the_ir, the_tvals, the_refine_spike_idx, the_config,the_peak_pcs_file_name,full_config);
-    all_cluster_sizes = cellfun(@length,cf);
-    % if any(all_cluster_sizes>30000)
-    %     disp("Cluster which will break produced")
-    % end
-    if the_config.DO_BAD_CLUSTER_ROUND && ~isempty(bad) %not sure why, but it will always skip the first time bad clusters are returned
-        for c = 1:length(bad) %cycle through all the bad clusters
-            cluster_idx = bad{c}; %get the idx for the bad clusters
-            cp = peaks(cluster_idx, :); % get the peaks for the current bad cluster
-            coeff = pca(cp); %get the pca coeff of the bad cluster peaks
-            the_peak_pcs = nan(size(peaks, 1), min([size(the_aligned_spikes,1),2])); %create a matrix which is #-peaks by #-channels, presumably to be filled in
-            for p = 1:size(the_peak_pcs,2) %cycle through the peak pcs matrix to fill in
-                the_peak_pcs(:, p) = peaks * coeff(:, p); %fill in the matrix with the cluster peaks times the pca coeff
-            end
-            data = [peaks, the_peak_pcs]; % structure the data which will be reclustered
-
-            ints = true(total_num_spikes, 1); %creates an array which seems to track which spikes pass the filtering test
-            for d = 1:size(data, 2) %cycle through each of the clustering features
-                feature = data(:, d); %get the current feature data
-                cluster_feature = feature(cluster_idx); %get all the feature data that belonds to the current cluster
-                d_min = min(cluster_feature); %get the min value of the cluster data
-                d_max = max(cluster_feature); %get the max value for the cluster feature
-
-                d_mean = mean(cluster_feature); %get the mean value of the cluster features
-                d_std = std(cluster_feature); %get the std of the cluster features
-
-                d_min = max(d_min, d_mean - num_std*d_std); %get the max value between the min cluster value and the cluster mean minus the config std (3 by default) * cluster std
-                d_max = min(d_max, d_mean + num_std*d_std); %get the min value between hte max cluster value and the cluster mean plus the config std (3 by default) * cluster std
-
-                ints = ints & d_min < feature & feature < d_max; %filter out any cluster features that don't fall between d_min and d_max
-            end
-            remove_spikes = union(find(ints), cluster_idx); %combine all the spikes that are in the bad cluster & the ones that don't pass the filter
-            filtered_idx = setdiff(filtered_idx, remove_spikes); %remove those spikes from the spikes that we use for clustering
-        end
-
-        % Run clustering again with bad spikes removed
-        [cf_bad,~,full_config,is_bad_cluster_for_bad_clusters]= run_clustering(the_aligned_spikes, filtered_idx, the_ir, the_tvals, the_refine_spike_idx, the_config,the_peak_pcs_file_name,full_config);
-    else
-        cf_bad = {};
-    end
-
-    % In case any of the new clusters overlap with the old ones, fix
-    % overlaps.
-    %the_clusters = fix_cluster_overlaps(the_aligned_spikes, [the_clusters, cf, cf_bad], the_config,full_config);
-    if isempty(cf_bad)
-        the_clusters = fix_cluster_overlaps(the_aligned_spikes, [the_clusters, cf(~is_bad_cluster),cf_bad], the_config,full_config);
-    else
-        the_clusters = fix_cluster_overlaps(the_aligned_spikes, [the_clusters, cf(~is_bad_cluster),cf_bad(~is_bad_cluster_for_bad_clusters)], the_config,full_config);
-    end
-    % disp("Length of Clusters" + string(length(clusters)));
-    bad = bad_tmp;
-    if full_config.has_ground_truth && full_config.debug_with_ground_truth
-        create_cluster_plots_with_accuracy_while_clustering_and_sub_clu(full_config,the_aligned_spikes,the_clusters,full_config.which_thresh,"after_fix_cluster_overlaps");
-        full_config.plot_counter = full_config.plot_counter +1;
-    end
-    % if ~isfile(full_config.base_aligned_sw_name)
-    %     par_save(full_config.base_aligned_sw_name,full_config.mutated_spike_windows);
-    % end
-end
+    % disp("finished and exited the core cluster while loop")
+    the_clusters = cellmap(@(x) x.idx, clusters); %returns the result of all subclustering
+    %result is a 1xnumber_of_clusters cell array where each item is the index of the spikes located in the kth cluster
 end
--- Utility_Functions/pipeline_testing_functions/test_tinkering_around_phase_using_toy_data.m
+++ Utility_Functions/pipeline_testing_functions/test_tinkering_around_phase_using_toy_data.m
@@ -3,13 +3,16 @@ function [] = test_tinkering_around_phase_using_toy_data(toy_data,ground_truth_i
 config.ART_TETR_ARRAY = reshape(1:100,[25,4]);
 clusters_for_toy_data = cell(size(config.ART_TETR_ARRAY,1),1);
 for i=1:size(config.ART_TETR_ARRAY,1)
-    current_peaks = toy_data(:,config.ACG_THRESHOLD(i,:));
+    current_peaks = toy_data(:,config.ART_TETR_ARRAY(i,:));
     z_sc_of_peaks = zscore(max(current_peaks, [], 2));
+    the_percentiles = prctile(z_sc_of_peaks,1:100);
+    
     snr_threshs = the_percentiles(config.percentiles_to_use);
     the_subsets = cell(1,length(snr_threshs));
-    for j=1:length(snr_threshs)
-        the_subsets{j} = snr
+    for j=1:length(snr_threshs)-1
+        the_subsets{j} = z_sc_of_peaks > snr_threshs;
     end
+    the_subsets{end} = ones(size(current_peaks,1),1);
     %we're just using percentiles of z score here not the full pmv, it's just approximated
     clusters_for_toy_data{i} = get_clusters_for_toy_data(the_subsets,current_peaks);
     
