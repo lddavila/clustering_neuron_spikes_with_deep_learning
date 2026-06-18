f2d71f8b28fdac4d9cd5d41498cb0d56283c89d9
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2026-06-18 15:46:28 -0600

    refactored the clustering schema

--- Utility_Functions/new_feature_testing/test_finding_core_dims_with_toy_data.m
+++ Utility_Functions/new_feature_testing/test_finding_core_dims_with_toy_data.m
@@ -36,44 +36,53 @@ disp("Finished getting toy data");
 all_combos = nchoosek(1:size(X,2),2);
 
 % for each combo find all units that are technically findable on those channels
-technically_findable_units = cell(size(all_combos,1),1);
-for i=1:size(all_combos,1)
-    current_dimensions = all_combos(i,:);
-    local_findable = nan(length(cell_array_of_cores),1);
-    for j=1:length(cell_array_of_cores)
-        current_clust_cores = cell_array_of_cores{j};
-        if any(ismember(current_clust_cores,current_dimensions))
-            local_findable(j) = j;
+if ~isfile(fullfile(dir_to_save_these_results_to,"technically_findable.mat"))
+    technically_findable_units = cell(size(all_combos,1),1);
+    for i=1:size(all_combos,1)
+        current_dimensions = all_combos(i,:);
+        local_findable = nan(length(cell_array_of_cores),1);
+        for j=1:length(cell_array_of_cores)
+            current_clust_cores = cell_array_of_cores{j};
+            if any(ismember(current_clust_cores,current_dimensions))
+                local_findable(j) = j;
+            end
         end
+        local_findable(isnan(local_findable)) = [];
+        technically_findable_units{i} = local_findable;
     end
-    local_findable(isnan(local_findable)) = [];
-    technically_findable_units{i} = local_findable;
+
+
+    par_save(fullfile(dir_to_save_these_results_to,"technically_findable.mat"),technically_findable_units);
+else
+    technically_findable_units = importdata(fullfile(dir_to_save_these_results_to,"technically_findable.mat"));
 end
-cluster = parcluster("Processes");
-num_workers = max(1, floor(cluster.NumWorkers / 4));
-fprintf("Have %i workers\n",num_workers);
-poolobj = parpool(cluster, num_workers);
 disp("Finished getting technically findable units");
-par_save(fullfile(dir_to_save_these_results_to,"technically_findable.mat"),technically_findable_units);
+
+% cluster = parcluster("Processes");
+% num_workers = max(1, floor(cluster.NumWorkers / 4));
+% fprintf("Have %i workers\n",num_workers);
+poolobj = parpool("Processes",8);
+%save the waveform data into files that match all_combos so that we can
+%effeciently run the clustering in a parfor loop
+dir_with_sliced_data = create_a_file_if_it_doesnt_exist_and_ret_abs_path(fullfile(dir_to_save_these_results_to,"sliced_toy_data"));
+for i=1:size(waveforms,1)
+    dim_i_data = abs(X(:,i));
+    dim_i_wf = waveforms(i,:,:);
+    data_struct = struct();
+    data_struct.wf = dim_i_wf;
+    data_struct.peaks = dim_i_data;
+    par_save(fullfile(dir_with_sliced_data,"dim_"+string(i)+".mat"));
+    disp("Finished saving "+string(i)+"/"+size(waveforms,1));
+end
+
 % now run the clustering for each of those 2 dimensional combos to see what units are findable through clusters
 % I THINK it would be best to track this on per neuron basis so we'll start
 % with getting the results from assembling every pair of 2
 cell_array_of_toy_clusters_tables = cell(size(all_combos,1),1);
-[toy_clusters,tetrode_arrays] = test_tinkering_around_phase_using_toy_data(X,cell_array_of_gt_idxs,cell_array_of_cores,config,waveforms,all_combos,dir_to_save_these_results_to);
+[toy_clusters,tetrode_arrays] = test_tinkering_around_phase_using_toy_data(X,cell_array_of_gt_idxs,cell_array_of_cores,config,all_combos,dir_to_save_these_results_to,dir_with_sliced_data);
 toy_clusters_table = table(vertcat(toy_clusters{:}),(1:1:size(tetrode_arrays,1)).',tetrode_arrays,'VariableNames',["cluster_idx","cluster","channels"]);
 [toy_clusters_table] = get_cluster_accuracy_for_toy_data(toy_clusters_table,cell_array_of_gt_idxs,cell_array_of_cores);
 disp("Finished getting the toy clusters table");
-
-% for i = 1:size(all_combos,1)
-%     current_dimensions = all_combos(i,:);
-%
-%     toy_clusters_table = table(vertcat(toy_clusters{:}),tetrode_arrays,'VariableNames',["cluster_idx","channels"]);
-%     % get which cluster represents which toy ground truth unit
-%     [toy_clusters_table] = get_cluster_accuracy_for_toy_data(toy_clusters_table,cell_array_of_gt_idxs,cell_array_of_cores);
-%     cell_array_of_toy_clusters_tables{i} = toy_clusters_table;
-%     send(q,[])
-% end
-
 
 
 
--- Utility_Functions/pipeline_testing_functions/test_tinkering_around_phase_using_toy_data.m
+++ Utility_Functions/pipeline_testing_functions/test_tinkering_around_phase_using_toy_data.m
@@ -1,19 +1,8 @@
-function [clusters_for_toy_data,dimensions_per_cluster] = test_tinkering_around_phase_using_toy_data(toy_data,ground_truth_idxs,ground_truth_cores,config,all_wf,varargin)
+function [clusters_for_toy_data,dimensions_per_cluster] = test_tinkering_around_phase_using_toy_data(toy_data,ground_truth_idxs,ground_truth_cores,config,art_tetr_array,save_img_dir,dir_with_sliced_data)
 %create tetrodes from the toy data dimensions
 
-if isempty(varargin)
-    config.ART_TETR_ARRAY = reshape(1:size(toy_data,2),[],4);
-    art_tetr_array = reshape(1:size(toy_data,2),[],4);
-else
-    config.ART_TETR_ARRAY = varargin{1};
-    art_tetr_array = varargin{1};
-end
-clusters_for_toy_data = cell(size(config.ART_TETR_ARRAY,1),1);
-if isempty(varargin)
-    save_img_dir = create_a_file_if_it_doesnt_exist_and_ret_abs_path(fullfile(config.parent_save_dir,"pairs_of_2_plots"));
-else
-    save_img_dir = varargin{2};
-end
+clusters_for_toy_data = cell(size(art_tetr_array,1),1);
+
 art_tetr_dir = create_a_file_if_it_doesnt_exist_and_ret_abs_path(fullfile(save_img_dir,"tetrode"));
 true_core_dir = create_a_file_if_it_doesnt_exist_and_ret_abs_path(fullfile(save_img_dir,"ground_truth"));
 clustering_dir = create_a_file_if_it_doesnt_exist_and_ret_abs_path(fullfile(save_img_dir,"clustering_results"));
@@ -52,105 +41,121 @@ for i=1:length(ground_truth_idxs)
     disp(i);
 end
 
-%slice data for parallel processing
-% sliced_toy_data = cell(size(art_tetr_array,1));
-% sliced_wf_data = cell(size(art_tetr_array,1));
-% for i=1:size(art_tetr_array,1)
-%     sliced_toy_data{i} = abs(toy_data(:,art_tetr_array(i,:)));
-%     sliced_wf_data{i} = all_wf(art_tetr_array(i,:),:,:);
-%     disp("finished slicing "+string(i)+"/"+string(size(art_tetr_array,1)));
-% end
+
+% dir_with_sliced_data = varargin{3};
+temp_table = table(art_tetr_array(:,1),art_tetr_array(:,2),"VariableNames",["dim_1","dim_2"]);
+sliced_temp_table = slice_table_for_parallel_processing(temp_table,"dim_1");
+
 
 q = parallel.pool.DataQueue;
 afterEach(q,@print_status_bar)
-num_iterations = size(config.ART_TETR_ARRAY,1);
+num_iterations = size(art_tetr_array,1);
+
+
 print_status_bar(num_iterations,"test_tinkering_around_phase_using_toy_data.m")
-parfor i=1:size(config.ART_TETR_ARRAY,1)
-    save_name = fullfile(art_tetr_dir,"Tetrode "+string(i));
+starting_point=1;
+for i=1:length(sliced_temp_table)
 
-    cluster_save_name = fullfile(clustering_dir,"Tetrode "+string(i)+".mat");
+    current_data = sliced_temp_table{i};
 
-    if ~isfile(cluster_save_name)
-        current_peaks = abs(toy_data(:,art_tetr_array(i,:))); %get the peaks for the current tetrode
-        current_waveforms = all_wf(art_tetr_array(i,:),:,:);
-        %of these dimensions check how many clusters appear on them
-        % number_of_clusters = 0;
-        current_waveforms = align_to_peak_ver_2(current_waveforms);
-        local_config = config;
-        local_config.current_channels = config.ART_TETR_ARRAY(i,:);
-        current_channels = config.ART_TETR_ARRAY(i,:);
-        % for j=1:length(ground_truth_cores)
-        %     if sum(ismember(config.ART_TETR_ARRAY(i,:),ground_truth_cores{j})) >=2
-        %         number_of_clusters = number_of_clusters+1;
-        %     end
-        % end
-        % fprintf("# of clusters that appear on this tetrode %i\n",number_of_clusters);
-        % fprintf("# of clusters found by dbscan: %i\n",length(unique(clusters_for_toy_data{i}))-1)
+    dim_1 = current_data{1,"dim_1"};
+    dim_1_struct = load(fullfile(dir_with_sliced_data,"dim_"+string(dim_1)+".mat"),"data_struct");
+    dim_1_struct = dim_1_struct.data_struct;
+    local_clusters_for_toy_data = cell(height(current_data),1);
+    local_dims_per_cluster = cell(height(current_data),1);
+    dim_1_struct_parallel = parallel.pool.Constant(dim_1_struct);
+    parfor j=1:height(current_data)
+        current_channels = current_data{j,:};
+        first_dim = current_channels(1);
+        dim_2 = current_channels(2);
+        save_name = fullfile(art_tetr_dir,"dims_"+string(first_dim)+"_"+string(dim_2));
+        cluster_save_name = fullfile(clustering_dir,"dims_"+string(first_dim)+"_"+string(dim_2)+".mat");
+        
 
+        if ~isfile(cluster_save_name)
+            dim_2_struct = load(fullfile(dir_with_sliced_data,"dim_"+string(dim_2)+".mat"),"data_struct");
+            dim_2_struct = dim_2_struct.data_struct;
+            current_peaks = [dim_1_struct_parallel.Value.peaks,dim_2_struct.peaks];
+            current_waveforms = [dim_1_struct_parallel.Value.wf;dim_2_struct.wf];
 
-        % current_peaks = toy_data(:,config.ART_TETR_ARRAY(i,:));
-        z_sc_of_peaks = zscore(max(current_peaks, [], 2));
-        the_percentiles = prctile(z_sc_of_peaks,1:100);
-
-        snr_threshs = the_percentiles(config.percentiles_to_use);
-        the_subsets = cell(1,length(snr_threshs));
-        for j=1:length(snr_threshs)-1
-            the_subsets{j} = z_sc_of_peaks > snr_threshs(j);
-        end
-        the_subsets{end} = ones(size(current_peaks,1),1);
-        %we're just using percentiles of z score here not the full pmv, it's just approximated
-        tvals = mean(current_peaks) + (std(current_peaks)* config.NUM_OF_STD_ABOVE_MEAN);
-        % tvals = [100 105 110 120];
-        ir = [100;130;150;100];
-        local_clusters = get_clusters_for_toy_data(current_peaks,the_subsets,current_waveforms,local_config.spikesort,local_config,ir,tvals);
-        if size(local_clusters,2) > size(local_clusters,1)
-            local_clusters = local_clusters.';
-        end
-        clusters_for_toy_data{i} = local_clusters;
-        disp("Finished "+string(i)+"/"+string(size(config.ART_TETR_ARRAY,1)))
-        disp(clusters_for_toy_data(i))
+            %of these dimensions check how many clusters appear on them
+            % number_of_clusters = 0;
+            current_waveforms = align_to_peak_ver_2(current_waveforms);
+            local_config = config;
+            local_config.current_channels = current_data{j,:};
+            
 
 
-        all_comps = nchoosek(1:length(config.ART_TETR_ARRAY(i,:)),2);
-        f = figure('units', 'normalized', 'outerposition', [0 0 1 1],'Visible','off');
-        tiledlayout("flow");
-        cluster_colors = distinguishable_colors(length(clusters_for_toy_data{i}));
+            % current_peaks = toy_data(:,config.ART_TETR_ARRAY(i,:));
+            z_sc_of_peaks = zscore(max(current_peaks, [], 2));
+            the_percentiles = prctile(z_sc_of_peaks,1:100);
 
-        for k=1:size(all_comps,1)
-            dim_1 = all_comps(k,1);
-            dim_2 = all_comps(k,2);
-            x_data = current_peaks(:,dim_1);
-            y_data = current_peaks(:,dim_2);
+            snr_threshs = the_percentiles(config.percentiles_to_use);
+            the_subsets = cell(1,length(snr_threshs));
+            for s=1:length(snr_threshs)-1
+                the_subsets{s} = z_sc_of_peaks > snr_threshs(s);
+            end
+            the_subsets{end} = ones(size(current_peaks,1),1);
+            %we're just using percentiles of z score here not the full pmv, it's just approximated
+            tvals = mean(current_peaks) + (std(current_peaks)* config.NUM_OF_STD_ABOVE_MEAN);
+            % tvals = [100 105 110 120];
 
-            nexttile;
-            scatter(x_data,y_data,3,[0.7 0.7 0.7],'filled','DisplayName',"unclustered");
-            hold on;
-            for j=1:length(clusters_for_toy_data{i})
-                current_cluster_idxs = clusters_for_toy_data{i}{j};
-                scatter(x_data(current_cluster_idxs),y_data(current_cluster_idxs),3,cluster_colors(j,:),'filled','DisplayName',"cluster"+string(j));
+            ir = [100;130];
+
+            local_clusters = get_clusters_for_toy_data(current_peaks,the_subsets,current_waveforms,local_config.spikesort,local_config,ir,tvals);
+            if size(local_clusters,2) > size(local_clusters,1)
+                local_clusters = local_clusters.';
+            end
+            local_clusters_for_toy_data{j} = local_clusters;
+
+
+            all_comps = [1,2];%nchoosek(1:length(config.ART_TETR_ARRAY(i,:)),2);
+            f = figure('units', 'normalized', 'outerposition', [0 0 1 1],'Visible','off');
+            tiledlayout("flow");
+            cluster_colors = distinguishable_colors(length(local_clusters_for_toy_data{j}));
+
+            for k=1:size(all_comps,1)
+                dim_1 = all_comps(k,1);
+                dim_2 = all_comps(k,2);
+                x_data = current_peaks(:,dim_1);
+                y_data = current_peaks(:,dim_2);
+
+                nexttile;
+                scatter(x_data,y_data,3,[0.7 0.7 0.7],'filled','DisplayName',"unclustered");
                 hold on;
+                for c=1:length(local_clusters_for_toy_data{j})
+                    current_cluster_idxs = local_clusters_for_toy_data{j}{c};
+                    scatter(x_data(current_cluster_idxs),y_data(current_cluster_idxs),3,cluster_colors(c,:),'filled','DisplayName',"cluster"+string(c));
+                    hold on;
+                end
+                xlabel("simulated dimension "+string(current_channels(dim_1)) +" (in \muV)")
+                ylabel("simulated dimension "+string(current_channels(dim_2)) +" (in \muV)")
+
+                if k==size(all_comps,1)
+                    legend;
+                end
+
+
             end
-            xlabel("simulated dimension "+string(current_channels(dim_1)) +" (in \muV)")
-            ylabel("simulated dimension "+string(current_channels(dim_2)) +" (in \muV)")
-
-            if k==size(all_comps,1)
-                legend;
+            save_plots_in_all_formats(f,save_name);
+            close(f);
+            par_save(cluster_save_name,local_clusters_for_toy_data{j});
+        else
+            local_clusters = importdata(cluster_save_name);
+            if size(local_clusters,2) > size(local_clusters,1)
+                local_clusters = local_clusters.';
             end
-
-
+            local_clusters_for_toy_data{j} = local_clusters;
         end
-        save_plots_in_all_formats(f,save_name);
-        close(f);
-        par_save(cluster_save_name,clusters_for_toy_data{i});
-    else
-        local_clusters = importdata(cluster_save_name);
-        if size(local_clusters,2) > size(local_clusters,1)
-            local_clusters = local_clusters.';
-        end
-        clusters_for_toy_data{i} = local_clusters;
+        local_dims_per_cluster{j} = repmat(current_channels,length(local_clusters),1);
+
+        send(q,[]);
     end
-    dimensions_per_cluster = [dimensions_per_cluster;repmat(art_tetr_array(i,:),length(local_clusters),1)];
-    send(q,[]);
+    dimensions_per_cluster = [dimensions_per_cluster;vertcat(local_dims_per_cluster{:})];
+    clusters_for_toy_data(starting_point:(starting_point+height(current_data))-1) = local_clusters_for_toy_data;
+    starting_point = starting_point + height(current_data);
+
+
 end
 
 end
