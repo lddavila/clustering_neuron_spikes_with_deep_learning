648f93a5e1b66ac06395c4556d80c76763c7ef18
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2026-06-18 00:28:27 -0600

    added submit script

---
+++ Utility_Functions/new_feature_testing/ss_frontera_test_finding_core_dims_with_toy_data.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+#SBATCH -N 1
+#SBATCH -p small
+#SBATCH -J tst_10
+#SBATCH -o output.txt 
+#SBATCH -e output.txt 
+#SBATCH -t 48:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=lddavila@miners.utep.edu
+#SBATCH -A ASC25063
+module load matlab/2023b
+matlab -batch "test_finding_core_dims_with_toy_data();exit;"
