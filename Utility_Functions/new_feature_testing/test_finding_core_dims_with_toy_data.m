5f607c0d2079a0fda6a525dfa44d7007e1e15f94
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2026-06-18 09:48:36 -0600

    throttled num workers

--- Utility_Functions/new_feature_testing/test_finding_core_dims_with_toy_data.m
+++ Utility_Functions/new_feature_testing/test_finding_core_dims_with_toy_data.m
@@ -27,7 +27,7 @@ disp("Finished getting toy data");
 % [toy_clusters_table] = get_cluster_accuracy_for_toy_data(toy_clusters_table,cell_array_of_gt_idxs,cell_array_of_cores);
 
 % try a computational approach
-% we want to know how these clusters show up with the clustering algorithm 
+% we want to know how these clusters show up with the clustering algorithm
 % and if there's an ideal number of dimensions that maximizes accuracy
 % I think the method I tried above wasn't worthwhile
 % I think it might be better instead to design it in the following way
@@ -49,11 +49,15 @@ for i=1:size(all_combos,1)
     local_findable(isnan(local_findable)) = [];
     technically_findable_units{i} = local_findable;
 end
+cluster = parcluster("Processes");
+num_workers = max(1, floor(cluster.NumWorkers / 4));
+fprintf("Have %i workers\n",num_workers);
+poolobj = parpool(cluster, num_workers);
 disp("Finished getting technically findable units");
 par_save(fullfile(dir_to_save_these_results_to,"technically_findable.mat"),technically_findable_units);
 % now run the clustering for each of those 2 dimensional combos to see what units are findable through clusters
 % I THINK it would be best to track this on per neuron basis so we'll start
-% with getting the results from assembling every pair of 2 
+% with getting the results from assembling every pair of 2
 cell_array_of_toy_clusters_tables = cell(size(all_combos,1),1);
 [toy_clusters,tetrode_arrays] = test_tinkering_around_phase_using_toy_data(X,cell_array_of_gt_idxs,cell_array_of_cores,config,waveforms,all_combos,dir_to_save_these_results_to);
 toy_clusters_table = table(vertcat(toy_clusters{:}),(1:1:size(tetrode_arrays,1)).',tetrode_arrays,'VariableNames',["cluster_idx","cluster","channels"]);
@@ -62,7 +66,7 @@ disp("Finished getting the toy clusters table");
 
 % for i = 1:size(all_combos,1)
 %     current_dimensions = all_combos(i,:);
-% 
+%
 %     toy_clusters_table = table(vertcat(toy_clusters{:}),tetrode_arrays,'VariableNames',["cluster_idx","channels"]);
 %     % get which cluster represents which toy ground truth unit
 %     [toy_clusters_table] = get_cluster_accuracy_for_toy_data(toy_clusters_table,cell_array_of_gt_idxs,cell_array_of_cores);
