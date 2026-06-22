f8b6cfbd063de5a1202f3bfc47b244a2ab0a3fd5
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2026-06-22 07:37:45 -0600

    further throttled num workers

--- Utility_Functions/new_feature_testing/test_ic_on_rec_10.m
+++ Utility_Functions/new_feature_testing/test_ic_on_rec_10.m
@@ -16,7 +16,7 @@ default_dir_parts = ["_600Neuron300SecondRecordingWithLevel","Noise"];
 
 if ~isempty(varargin)
     cluster = parcluster("Processes");
-    num_workers = max(1, floor(cluster.NumWorkers / 4));
+    num_workers = max(1, floor(cluster.NumWorkers / 8));
     fprintf("Have %i workers\n",num_workers);
     poolobj = parpool(cluster, num_workers);
 
