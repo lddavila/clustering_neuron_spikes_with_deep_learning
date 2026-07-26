d3bcc7c974972013e6a7190061f2b1cbe3d6b9d0
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2026-07-26 00:44:16 -0600

    changed test

--- Utility_Functions/new_feature_testing/test_cluster_splitting.m
+++ Utility_Functions/new_feature_testing/test_cluster_splitting.m
@@ -17,7 +17,7 @@ if contains(pwd,"10595")
     config.GT_FP = fullfile(config.base_file_path,"Data",config.RECORDING_NAME,"ground_truth","ground_truth.mat");
     config.TIMESTAMP_FP = fullfile(config.base_file_path,"Data",config.RECORDING_NAME,"timestamps","timestamps.mat");
     config.DIR_WITH_OG_CHANNEL_RECORDINGS = fullfile(config.base_file_path,"Data",config.RECORDING_NAME,"recordings_by_channel");
-    blind_pass_table = importdata("/scratch2/10595/lddavila/clustering_neuron_spikes_with_deep_learning/Default_Results_Dir/all_pmv_10_600Neuron300SecondRecordingWithLevel10Noise_4_ch_07_19_2026/blind_pass_table.mat");
+    blind_pass_table = importdata("/scratch2/10595/lddavila/clustering_neuron_spikes_with_deep_learning/Default_Results_Dir/temp_dir/blind_pass_table.mat");
 elseif contains(pwd,"C:\Users\ldd77\")
     % ext_drive_fp = "F:";
     config.GT_FP = fullfile(config.base_file_path,"Data",config.RECORDING_NAME,"ground_truth","ground_truth.mat");
@@ -34,7 +34,7 @@ config.has_ground_truth = true;
 config.BLIND_PASS_DIR_PRECOMPUTED = create_a_file_if_it_doesnt_exist_and_ret_abs_path(fullfile(config.BLIND_PASS_DIR_PRECOMPUTED,"testing_cluster_splitting_population_attempt_2"));
 
 config.error_dir = create_a_file_if_it_doesnt_exist_and_ret_abs_path(fullfile(config.BLIND_PASS_DIR_PRECOMPUTED,"error_reports"));
-bp_table_after_splitting_save_name = fullfile(config.BLIND_PASS_DIR_PRECOMPUTED,"bp_table_after_splitting.mat");
+bp_table_after_splitting_save_name = fullfile("/scratch2/10595/lddavila/clustering_neuron_spikes_with_deep_learning/Default_Results_Dir/temp_dir","bp_table_after_splitting.mat");
 
 if ~isfile(bp_table_after_splitting_save_name)
     bp_table_after_splitting = split_clusters_with_alt_dimensions(blind_pass_table,config,'plot_the_debug',true);
@@ -56,37 +56,37 @@ end
 
 
 %extract grades from bp_table_after_splitting
-list_of_features_to_add = ["grades 3"];
-grades_array = [cell2mat(assemble_data_for_neural_net(list_of_features_to_add,bp_table_after_splitting,config))];
-table_of_nets = struct2table(dir(fullfile(config.dir_of_prob_dist_nets,"*.mat")));
-net_names = string(table_of_nets.name);
-split_net_names = split(net_names,"_");
-[~,where_below_ends ]= find(split_net_names=="below");
-net_nums = arrayfun(@(i) split_net_names(i, where_below_ends(i)+1), ...
-    (1:size(split_net_names,1))');
-
-table_of_nets.threshold = str2double(net_nums);
-table_of_nets = sortrows(table_of_nets,"threshold","ascend");
-
-dir_to_nn_sets = "C:\Users\ldd77\clustering_neuron_spikes_with_deep_learning\Default_Results_Dir\probability_distr_nets_equalized_difficulty_grades_3_with_temp_scaling";
-true_accuracy = bp_table_after_splitting{:,"accuracy"};
-f = figure;
-tiledlayout('flow');
-nexttile();
-histogram(true_accuracy,'BinEdges',1:1:100);
-ylabel("Frequency")
-xlabel("Accuracy")
-
-
-[~,unscaled_certainties ]= get_certainties_of_all_previous_nets(string(table_of_nets.name),dir_to_nn_sets,grades_array);
-all_positive =all(unscaled_certainties>0,2) | sum(unscaled_certainties>0,2)>80 ;
-all_negative = all(unscaled_certainties<0,2) | sum(unscaled_certainties<0,2)>80;
-to_eliminate = all_positive | all_negative;
-true_accuracy(to_eliminate) = [];
-unscaled_certainties(to_eliminate,:) = [];
-
-nexttile();
-histogram(true_accuracy,'BinEdges',1:1:100)
-ylabel("Frequency")
-xlabel("Accuracy")
+% list_of_features_to_add = ["grades 3"];
+% grades_array = [cell2mat(assemble_data_for_neural_net(list_of_features_to_add,bp_table_after_splitting,config))];
+% table_of_nets = struct2table(dir(fullfile(config.dir_of_prob_dist_nets,"*.mat")));
+% net_names = string(table_of_nets.name);
+% split_net_names = split(net_names,"_");
+% [~,where_below_ends ]= find(split_net_names=="below");
+% net_nums = arrayfun(@(i) split_net_names(i, where_below_ends(i)+1), ...
+%     (1:size(split_net_names,1))');
+% 
+% table_of_nets.threshold = str2double(net_nums);
+% table_of_nets = sortrows(table_of_nets,"threshold","ascend");
+% 
+% dir_to_nn_sets = "C:\Users\ldd77\clustering_neuron_spikes_with_deep_learning\Default_Results_Dir\probability_distr_nets_equalized_difficulty_grades_3_with_temp_scaling";
+% true_accuracy = bp_table_after_splitting{:,"accuracy"};
+% f = figure;
+% tiledlayout('flow');
+% nexttile();
+% histogram(true_accuracy,'BinEdges',1:1:100);
+% ylabel("Frequency")
+% xlabel("Accuracy")
+% 
+% 
+% [~,unscaled_certainties ]= get_certainties_of_all_previous_nets(string(table_of_nets.name),dir_to_nn_sets,grades_array);
+% all_positive =all(unscaled_certainties>0,2) | sum(unscaled_certainties>0,2)>80 ;
+% all_negative = all(unscaled_certainties<0,2) | sum(unscaled_certainties<0,2)>80;
+% to_eliminate = all_positive | all_negative;
+% true_accuracy(to_eliminate) = [];
+% unscaled_certainties(to_eliminate,:) = [];
+% 
+% nexttile();
+% histogram(true_accuracy,'BinEdges',1:1:100)
+% ylabel("Frequency")
+% xlabel("Accuracy")
 end
