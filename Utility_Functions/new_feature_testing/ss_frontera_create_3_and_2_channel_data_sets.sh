b39dc2f306a31e2a716ccbd5062909381e22f26a
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2026-08-09 23:52:00 -0600

    fixed typo

--- Utility_Functions/new_feature_testing/ss_frontera_create_3_and_2_channel_data_sets.sh
+++ Utility_Functions/new_feature_testing/ss_frontera_create_3_and_2_channel_data_sets.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 #SBATCH -N 1
 #SBATCH -p small
-#SBATCH -J nn_train
-#SBATCH -o output.txt 
-#SBATCH -e output.txt 
+#SBATCH -J sample_set
+#SBATCH -o output_2.txt 
+#SBATCH -e output_2.txt 
 #SBATCH -t 24:00:00
 module load matlab/2023b
-matlab -batch "create_3_and_2_channel_data_sets(10,1,'z'355);exit;"
+matlab -batch "create_3_and_2_channel_data_sets(10,1,'z');exit;"
