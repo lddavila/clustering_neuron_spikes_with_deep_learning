fa816cca641b0d619a4a8e60fe9a82e2f987687b
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2026-09-04 14:17:46 -0600

    changed runtime params

--- Utility_Functions/new_feature_testing/ss_ls6_create_test_sets_with_varying_channels_4_ch.sh
+++ Utility_Functions/new_feature_testing/ss_ls6_create_test_sets_with_varying_channels_4_ch.sh
@@ -2,9 +2,9 @@
 #SBATCH -N 1
 #SBATCH -p normal
 #SBATCH -J 4_ch
-#SBATCH -o output_4.txt 
-#SBATCH -e output_4.txt 
-#SBATCH -t 10:00:00
+#SBATCH -o output_4_sub.txt 
+#SBATCH -e output_4_sub.txt 
+#SBATCH -t 1:00:00
 #SBATCH -A CCR26037
 module load matlab/2023b
 matlab -batch "create_test_sets_with_varying_channels(10,4);exit;"
