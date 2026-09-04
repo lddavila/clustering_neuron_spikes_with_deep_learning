6ccb30324b4662296694ff20d2fc3a80ae0b11eb
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2026-09-04 14:48:48 -0600

    reduced runtime

--- Utility_Functions/new_feature_testing/ss_ls6_create_test_sets_with_varying_channels_4_ch.sh
+++ Utility_Functions/new_feature_testing/ss_ls6_create_test_sets_with_varying_channels_4_ch.sh
@@ -4,7 +4,7 @@
 #SBATCH -J 4_ch_sub
 #SBATCH -o output_4_sub.txt 
 #SBATCH -e output_4_sub.txt 
-#SBATCH -t 2:00:00
+#SBATCH -t 1:00:00
 #SBATCH -A CCR26037
 module load matlab/2023b
 matlab -batch "create_test_sets_with_varying_channels(10,4);exit;"
