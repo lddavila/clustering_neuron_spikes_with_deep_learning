b0b1d6c4834c665b34bb5f63c4f09b84c357b4fd
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2026-09-04 14:20:22 -0600

    decreased run time

--- Utility_Functions/new_feature_testing/ss_ls6_run_full_pass_4_ch.sh
+++ Utility_Functions/new_feature_testing/ss_ls6_run_full_pass_4_ch.sh
@@ -4,7 +4,7 @@
 #SBATCH -J 4_ch
 #SBATCH -o output_4.txt 
 #SBATCH -e output_4.txt 
-#SBATCH -t 48:00:00
+#SBATCH -t 1:00:00
 #SBATCH -A CCR26037
 module load matlab/2023b
 matlab -batch "run_full_pass(10,4,10);exit;"
