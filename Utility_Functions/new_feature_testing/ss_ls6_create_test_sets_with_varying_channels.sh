768733a8afd6f562c199bf58f397b0737464f73d
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2026-09-05 22:02:54 -0600

    updated runtime

--- Utility_Functions/new_feature_testing/ss_ls6_create_test_sets_with_varying_channels.sh
+++ Utility_Functions/new_feature_testing/ss_ls6_create_test_sets_with_varying_channels.sh
@@ -2,8 +2,8 @@
 #SBATCH -N 1
 #SBATCH -p normal
 #SBATCH -J 2_ch_sub
-#SBATCH -o create_test_sets_with_var_ch_output.txt 
-#SBATCH -e create_test_sets_with_var_ch_output.txt 
+#SBATCH -o output_2_sub.txt 
+#SBATCH -e ouput_2_sub.txt 
 #SBATCH -t 48:00:00
 #SBATCH -A CCR26037
 module load matlab/2023b
